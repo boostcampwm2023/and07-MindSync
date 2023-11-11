18acf8dc9e2d986f491f50eb9da863bc824265f2
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-11-11 18:55:37 +0900

    feat: ktlint action test

--- AOS/app/src/main/java/boostcamp/and07/mindsync/MainActivity.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (true) "3"
+        else "4"
     }
 }
