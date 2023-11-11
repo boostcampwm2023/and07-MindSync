a70291f5fc59f487fca3696ca5c335cefd2967f5
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-11-11 18:46:44 +0900

    feat: ktlint action test

--- AOS/app/src/main/java/boostcamp/and07/mindsync/MainActivity.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/MainActivity.kt
@@ -1,11 +1,13 @@
 package boostcamp.and07.mindsync
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (true) "3"
     }
 }
