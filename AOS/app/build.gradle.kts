e1794f8f8c16d1c6883c5eb2e0ff020d55c5b4d3
jaehan4707 <jahan4707@naver.com>
2023-11-13 17:11:29 +0900

    chore(#25): minSdk 26으로 변경
    Co-authored-by: hegleB <dltkd13956@naver.com>
    Co-authored-by: yang1318 <amy1353@naver.com>

--- AOS/app/build.gradle.kts
+++ AOS/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "boostcamp.and07.mindsync"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
