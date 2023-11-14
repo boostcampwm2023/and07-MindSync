579cc91281c5fd11943c6099e7af2da4f06d82b1
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-11-14 19:30:51 +0900

    refactor(#7): ColorRGB 값 int로 변경
    Co-authored-by: jaehan4707 <jahan4707@naver.com>

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/SampleNode.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/SampleNode.kt
@@ -1,51 +1,53 @@
 package boostcamp.and07.mindsync.data
 
+import boostcamp.and07.mindsync.data.model.CircleNode
 import boostcamp.and07.mindsync.data.model.CirclePath
 import boostcamp.and07.mindsync.data.model.ColorRGB
 import boostcamp.and07.mindsync.data.model.Node
+import boostcamp.and07.mindsync.data.model.RectangleNode
 import boostcamp.and07.mindsync.data.model.RectanglePath
 
 object SampleNode {
-    val head: Node = Node(
+    val head: Node = CircleNode(
         CirclePath(50f, 50f, 75f),
-        ColorRGB(100f, 100f, 100f),
+        ColorRGB(100, 100, 100),
         "Root",
         listOf(
-            Node(
+            RectangleNode(
                 RectanglePath(125f, 225f, 225f, 325f),
-                ColorRGB(0f, 0f, 0f),
+                ColorRGB(0, 0, 0),
                 "Child1",
                 listOf(
-                    Node(
+                    RectangleNode(
                         RectanglePath(125f, 225f, 225f, 325f),
-                        ColorRGB(0f, 0f, 0f),
+                        ColorRGB(0, 0, 0),
                         "Child3",
                         listOf(
-                            Node(
+                            RectangleNode(
                                 RectanglePath(125f, 225f, 225f, 325f),
-                                ColorRGB(0f, 0f, 0f),
+                                ColorRGB(0, 0, 0),
                                 "Child5",
                                 listOf(),
                             ),
-                            Node(
+                            RectangleNode(
                                 RectanglePath(125f, 225f, 425f, 525f),
-                                ColorRGB(0f, 0f, 0f),
+                                ColorRGB(0, 0, 0),
                                 "Child6",
                                 listOf(),
                             ),
                         )
                     ),
-                    Node(
+                    RectangleNode(
                         RectanglePath(125f, 225f, 425f, 525f),
-                        ColorRGB(0f, 0f, 0f),
+                        ColorRGB(0, 0, 0),
                         "Child4",
                         listOf(),
                     ),
                 )
             ),
-            Node(
+            RectangleNode(
                 RectanglePath(125f, 225f, 425f, 525f),
-                ColorRGB(0f, 0f, 0f),
+                ColorRGB(0, 0, 0),
                 "Child2",
                 listOf(),
             ),
