341b937e23cd4d4303f51bd2fe29329ee29d9a49
dltkd1395 <dltkd13956@naver.com>
2023-11-14 20:01:24 +0900

    feat(#5): 라인 데이터 구조 클래스 만들기

---
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/model/LinePath.kt
@@ -0,0 +1,8 @@
+package boostcamp.and07.mindsync.data.model
+
+data class LinePath(
+    val fromCenterX: Float,
+    val fromCenterY: Float,
+    val toCenterY: Float,
+    val toCenterX: Float,
+)
