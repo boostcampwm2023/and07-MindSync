9efa952d3327c7801dbcce22ee170b12ba1c2fa2
jaehan4707 <jahan4707@naver.com>
2023-11-16 18:17:55 +0900

    feat(#39): Node 더블 클릭 이벤트 구현
    더블 클릭 시 해당 지점에 노드가 있다면 다이얼로그를 출력

--- AOS/app/src/main/java/boostcamp/and07/mindsync/MainActivity.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/MainActivity.kt
@@ -1,11 +1,32 @@
 package boostcamp.and07.mindsync
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import boostcamp.and07.mindsync.data.model.Node
+import boostcamp.and07.mindsync.databinding.ActivityMainBinding
+import boostcamp.and07.mindsync.ui.base.BaseActivity
+import boostcamp.and07.mindsync.ui.view.NodeClickListener
+import boostcamp.and07.mindsync.ui.dialog.EditDescriptionDialog
+import boostcamp.and07.mindsync.ui.dialog.EditDialogInterface
 
-class MainActivity : AppCompatActivity() {
+class MainActivity :
+    BaseActivity<ActivityMainBinding>(R.layout.activity_main),
+    NodeClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+    }
+
+    override fun init() {
+        binding.customNodeView.setTextViewClickListener(this)
+    }
+
+    override fun onDoubleClicked(node: Node) {
+        val dialog = EditDescriptionDialog(this)
+        dialog.setListener(object : EditDialogInterface {
+            override fun onSubmitClick(description: String) {
+            }
+        })
+        dialog.show()
     }
 }
--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/view/NodeView.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/view/NodeView.kt
@@ -2,14 +2,18 @@ package boostcamp.and07.mindsync.ui.view
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 import boostcamp.and07.mindsync.R
 import boostcamp.and07.mindsync.data.SampleNode
 import boostcamp.and07.mindsync.data.model.CircleNode
 import boostcamp.and07.mindsync.data.model.Node
 import boostcamp.and07.mindsync.data.model.RectangleNode
+import boostcamp.and07.mindsync.ui.util.Dp
 import boostcamp.and07.mindsync.ui.util.toPx
 
 class NodeView constructor(context: Context, attrs: AttributeSet?) : View(context, attrs) {
@@ -25,21 +29,90 @@ class NodeView constructor(context: Context, attrs: AttributeSet?) : View(contex
         context.getColor(R.color.mindmap4),
         context.getColor(R.color.mindmap5),
     )
+    private val textPaint = Paint().apply {
+        color = Color.RED
+        textSize = Dp(12f).toPx(context).toFloat()
+        typeface = ResourcesCompat.getFont(context, R.font.pretendard_bold)
+    }
+    private var listener: NodeClickListener? = null
+
+    private var lastClickTime = 0L
+    fun setTextViewClickListener(listener: NodeClickListener) {
+        this.listener = listener
+    }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        traverseHead(canvas)
+        traverseDrawHead(canvas)
     }
 
-    private fun traverseHead(canvas: Canvas) {
-        traverseNode(canvas, head, 0)
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                val clickTime = System.currentTimeMillis()
+                if (clickTime - lastClickTime < DOUBLE_CLICK_TIME) {
+                    traverseRangeHead(event.x, event.y)
+                } else {
+                    lastClickTime = clickTime
+                    invalidate()
+                }
+            }
+        }
+        return super.onTouchEvent(event)
     }
 
-    private fun traverseNode(canvas: Canvas, node: Node, depth: Int) {
+    private fun traverseDrawHead(canvas: Canvas) {
+        traverseDrawNode(canvas, head, 0)
+    }
+
+    private fun traverseDrawNode(canvas: Canvas, node: Node, depth: Int) {
         drawNode(canvas, node, depth)
         node.nodes.forEach { node ->
-            traverseNode(canvas, node, depth + 1)
+            traverseDrawNode(canvas, node, depth + 1)
         }
+    }
+
+    private fun traverseRangeHead(x: Float, y: Float) {
+        val node = traverseRangeNode(head, x, y)
+        node?.let {
+            listener?.onDoubleClicked(node)
+        }
+    }
+
+    private fun traverseRangeNode(node: Node, x: Float, y: Float): Node? {
+        if (checkRange(node, x, y)) {
+            return node
+        }
+        for (child in node.nodes) {
+            return traverseRangeNode(child, x, y) ?: continue
+        }
+        return null
+    }
+
+    private fun checkRange(node: Node, x: Float, y: Float): Boolean {
+        when (node) {
+            is CircleNode -> {
+                if (x in (node.path.centerX - node.path.radius).toPx(context)
+                        .toFloat()..(node.path.centerX + node.path.radius).toPx(context)
+                        .toFloat() &&
+                    y in (node.path.centerY - node.path.radius).toPx(context)
+                        .toFloat()..(node.path.centerY + node.path.radius).toPx(context).toFloat()
+                ) {
+                    return true
+                }
+            }
+
+            is RectangleNode -> {
+                if (x in node.path.leftX().toPx(context).toFloat()..node.path.rightX().toPx(context)
+                        .toFloat() &&
+                    y in node.path.topY().toPx(context).toFloat()..node.path.bottomY()
+                        .toPx(context).toFloat()
+                ) {
+                    return true
+                }
+            }
+        }
+        return false
     }
 
     private fun drawNode(canvas: Canvas, node: Node, depth: Int) {
@@ -47,6 +120,7 @@ class NodeView constructor(context: Context, attrs: AttributeSet?) : View(contex
             is CircleNode -> drawCircleNode(canvas, node)
             is RectangleNode -> drawRectangleNode(canvas, node, depth)
         }
+        drawText(canvas, node, depth)
     }
 
     private fun drawCircleNode(canvas: Canvas, node: CircleNode) {
@@ -67,5 +141,35 @@ class NodeView constructor(context: Context, attrs: AttributeSet?) : View(contex
             node.path.bottomY().toPx(context).toFloat(),
             rectanglePaint,
         )
+    }
+
+    private fun drawText(canvas: Canvas, node: Node, depth: Int) {
+        val width = textPaint.measureText(node.description).toInt()
+        val height = textPaint.descent().toInt() - textPaint.ascent().toInt()
+        when (node) {
+            is CircleNode -> {
+                textPaint.color = Color.WHITE
+                canvas.drawText(
+                    node.description,
+                    node.path.centerX.toPx(context).toFloat() - (width / 2).toFloat(),
+                    node.path.centerY.toPx(context).toFloat() + (height / 2).toFloat(),
+                    textPaint,
+                )
+            }
+
+            is RectangleNode -> {
+                textPaint.color = Color.BLUE
+                canvas.drawText(
+                    node.description,
+                    node.path.centerX.toPx(context).toFloat() - (width / 2).toFloat(),
+                    node.path.centerY.toPx(context).toFloat() + (height / 2).toFloat(),
+                    textPaint,
+                )
+            }
+        }
+    }
+
+    companion object {
+        const val DOUBLE_CLICK_TIME = 200
     }
 }
