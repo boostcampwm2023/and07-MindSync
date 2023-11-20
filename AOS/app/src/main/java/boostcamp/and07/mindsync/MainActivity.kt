5e724b191c3ac60ebcec307b29523708711c4d5d
jaehan4707 <jahan4707@naver.com>
2023-11-20 21:58:42 +0900

    fix(#39): revert mainActivity,xml

--- AOS/app/src/main/java/boostcamp/and07/mindsync/MainActivity.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/MainActivity.kt
@@ -1,31 +1,11 @@
 package boostcamp.and07.mindsync
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import boostcamp.and07.mindsync.data.model.Node
-import boostcamp.and07.mindsync.databinding.ActivityMainBinding
-import boostcamp.and07.mindsync.ui.base.BaseActivity
-import boostcamp.and07.mindsync.ui.dialog.EditDescriptionDialog
-import boostcamp.and07.mindsync.ui.dialog.EditDialogInterface
-import boostcamp.and07.mindsync.ui.view.NodeClickListener
 
-class MainActivity :
-    BaseActivity<ActivityMainBinding>(R.layout.activity_main),
-    NodeClickListener {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-    }
-
-    override fun init() {
-        binding.customNodeView.setTextViewClickListener(this)
-    }
-
-    override fun onDoubleClicked(node: Node, color: Int) {
-        val dialog = EditDescriptionDialog(color)
-        dialog.setListener(object : EditDialogInterface {
-            override fun onSubmitClick(description: String) {
-            }
-        })
-        dialog.show(this.supportFragmentManager,"EditDescriptionDialog")
+        setContentView(R.layout.activity_main)
     }
 }
