a252d3e88fa8e2ad25ff65d6a24f28379d1423f1
jaehan4707 <jahan4707@naver.com>
2023-11-20 17:49:44 +0900

    feat(#39): 노드 크기 조절 자동화

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/view/NodeView.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/view/NodeView.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
+import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
@@ -14,10 +16,12 @@ import boostcamp.and07.mindsync.data.model.CircleNode
 import boostcamp.and07.mindsync.data.model.Node
 import boostcamp.and07.mindsync.data.model.RectangleNode
 import boostcamp.and07.mindsync.ui.util.Dp
+import boostcamp.and07.mindsync.ui.util.Px
+import boostcamp.and07.mindsync.ui.util.toDp
 import boostcamp.and07.mindsync.ui.util.toPx
 
 class NodeView constructor(context: Context, attrs: AttributeSet?) : View(context, attrs) {
-    private val head = SampleNode.head
+    private var head = SampleNode.head
     private val circlePaint = Paint().apply {
         color = context.getColor(R.color.mindmap1)
     }
@@ -29,33 +33,29 @@ class NodeView constructor(context: Context, attrs: AttributeSet?) : View(contex
         context.getColor(R.color.mindmap4),
         context.getColor(R.color.mindmap5),
     )
-    private val textPaint = Paint().apply {
+    private val textPaint = TextPaint().apply {
         color = Color.RED
         textSize = Dp(12f).toPx(context).toFloat()
+        isAntiAlias = true
         typeface = ResourcesCompat.getFont(context, R.font.pretendard_bold)
+        textAlign = Paint.Align.CENTER
     }
     private var listener: NodeClickListener? = null
-
-    private var lastClickTime = 0L
+    private val lineHeight = 15f
     fun setTextViewClickListener(listener: NodeClickListener) {
         this.listener = listener
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        traverseTextHead()
         traverseDrawHead(canvas)
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                val clickTime = System.currentTimeMillis()
-                if (clickTime - lastClickTime < DOUBLE_CLICK_TIME) {
-                    traverseRangeHead(event.x, event.y)
-                } else {
-                    lastClickTime = clickTime
-                    invalidate()
-                }
+                traverseRangeHead(event.x, event.y)
             }
         }
         return super.onTouchEvent(event)
@@ -72,19 +72,75 @@ class NodeView constructor(context: Context, attrs: AttributeSet?) : View(contex
         }
     }
 
-    private fun traverseRangeHead(x: Float, y: Float) {
-        val node = traverseRangeNode(head, x, y)
-        node?.let {
-            listener?.onDoubleClicked(node)
+    private fun traverseTextHead() {
+        head = traverseTextNode(head)
+    }
+
+    private fun traverseTextNode(node: Node): Node {
+        val newNodes = mutableListOf<Node>() // Node의 자식들.
+        node.nodes.forEach { child ->
+            newNodes.add(traverseTextNode(child))
+            traverseTextNode(child)
+        }
+        val copyNode =
+            changeSize(node, sumWidth(node.description), sumTotalHeight(node.description))
+        val newNode = when (copyNode) {
+            is CircleNode -> copyNode.copy(nodes = newNodes)
+            is RectangleNode -> copyNode.copy(nodes = newNodes)
+        }
+        return newNode
+    }
+
+    private fun changeSize(node: Node, width: Px, height: Float): Node {
+        when (node) {
+            is CircleNode -> {
+                var newRadius = node.path.radius
+                if (width.toDp(context)
+                        .toFloat() > node.path.radius.dpVal && !node.description.contains("\n")
+                ) {
+                    newRadius = Dp(width.toDp(context).toFloat() / 2) + Dp(lineHeight / 2)
+                }
+                if (node.description.contains("\n")) {
+                    newRadius = (Dp(height) - Dp(lineHeight)) / Dp(2f)
+                }
+                return node.copy(node.path.copy(radius = newRadius))
+            }
+
+            is RectangleNode -> {
+                var newWidth: Dp
+                if (width.toDp(context)
+                        .toFloat() > node.path.width.dpVal && !node.description.contains("\n")
+                ) {
+                    newWidth = Dp(width.toDp(context).toFloat()) + Dp(lineHeight)
+                    return node.copy(node.path.copy(width = newWidth))
+                }
+                var newHeight: Dp = Dp(height) / Dp(2f)
+                return node.copy(node.path.copy(height = newHeight))
+            }
         }
     }
 
-    private fun traverseRangeNode(node: Node, x: Float, y: Float): Node? {
+    private fun traverseRangeHead(x: Float, y: Float) {
+        val rangeResult = traverseRangeNode(head, x, y, 0)
+
+        rangeResult?.let {
+            when (it.first) {
+                is CircleNode -> listener?.onDoubleClicked(
+                    it.first,
+                    context.getColor(R.color.mindmap1),
+                )
+
+                is RectangleNode -> listener?.onDoubleClicked(it.first, nodeColors[it.second - 1])
+            }
+        }
+    }
+
+    private fun traverseRangeNode(node: Node, x: Float, y: Float, depth: Int): Pair<Node, Int>? {
         if (checkRange(node, x, y)) {
-            return node
+            return Pair(node, depth)
         }
         for (child in node.nodes) {
-            return traverseRangeNode(child, x, y) ?: continue
+            return traverseRangeNode(child, x, y, depth + 1) ?: continue
         }
         return null
     }
@@ -143,33 +199,69 @@ class NodeView constructor(context: Context, attrs: AttributeSet?) : View(contex
         )
     }
 
+    private fun sumTotalHeight(description: String): Float {
+        val bounds = Rect()
+        var sum = 0f
+        description.split("\n").forEach { line ->
+            textPaint.getTextBounds(line, 0, line.length, bounds)
+            sum += bounds.height() + lineHeight
+        }
+        return sum
+    }
+
+    private fun sumWidth(description: String) = Px(textPaint.measureText(description))
     private fun drawText(canvas: Canvas, node: Node) {
-        val width = textPaint.measureText(node.description).toInt()
-        val height = textPaint.descent().toInt() - textPaint.ascent().toInt()
+        val lines = node.description.split("\n")
+        var bounds = Rect()
+        var totalHeight = sumTotalHeight(node.description)
         when (node) {
             is CircleNode -> {
                 textPaint.color = Color.WHITE
-                canvas.drawText(
-                    node.description,
-                    node.path.centerX.toPx(context).toFloat() - (width / 2).toFloat(),
-                    node.path.centerY.toPx(context).toFloat() + (height / 2).toFloat(),
-                    textPaint,
-                )
+                if (lines.size > 1) {
+                    var y = node.path.centerY.toPx(context).toFloat() - totalHeight / 2
+                    for (line in lines) {
+                        textPaint.getTextBounds(line, 0, line.length, bounds)
+                        canvas.drawText(
+                            line,
+                            node.path.centerX.toPx(context).toFloat(),
+                            y + bounds.height(),
+                            textPaint,
+                        )
+                        y += bounds.height() + lineHeight
+                    }
+                } else {
+                    canvas.drawText(
+                        node.description,
+                        node.path.centerX.toPx(context).toFloat(),
+                        node.path.centerY.toPx(context).toFloat(),
+                        textPaint,
+                    )
+                }
             }
 
             is RectangleNode -> {
                 textPaint.color = Color.BLACK
-                canvas.drawText(
-                    node.description,
-                    node.path.centerX.toPx(context).toFloat() - (width / 2).toFloat(),
-                    node.path.centerY.toPx(context).toFloat() + (height / 2).toFloat(),
-                    textPaint,
-                )
+                if (lines.size > 1) {
+                    var y = node.path.centerY.toPx(context).toFloat() - totalHeight / 2
+                    for (line in lines) {
+                        textPaint.getTextBounds(line, 0, line.length, bounds)
+                        canvas.drawText(
+                            line,
+                            node.path.centerX.toPx(context).toFloat(),
+                            y + bounds.height(),
+                            textPaint,
+                        )
+                        y += bounds.height() + lineHeight
+                    }
+                } else {
+                    canvas.drawText(
+                        node.description,
+                        node.path.centerX.toPx(context).toFloat(),
+                        node.path.centerY.toPx(context).toFloat(),
+                        textPaint,
+                    )
+                }
             }
         }
-    }
-
-    companion object {
-        const val DOUBLE_CLICK_TIME = 200
     }
 }
