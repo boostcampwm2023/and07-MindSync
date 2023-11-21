990811a8b4a3b9e66b8ccdcf4302337edbebd5ac
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-11-21 19:06:15 +0900

    feat(#52): 뒤로가기 클릭시 drawer close & 뒤로가기 두번 클릭해야 종료

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/MainActivity.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/MainActivity.kt
@@ -1,21 +1,47 @@
 package boostcamp.and07.mindsync.ui
 
 import android.view.View
+import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import boostcamp.and07.mindsync.R
 import boostcamp.and07.mindsync.databinding.ActivityMainBinding
 import boostcamp.and07.mindsync.ui.base.BaseActivity
 
 class MainActivity :
-    BaseActivity<ActivityMainBinding>(boostcamp.and07.mindsync.R.layout.activity_main) {
+    BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private lateinit var drawerLayout: DrawerLayout
+    private var backPressedTime: Long = 0L
+    private var backPressedToast: Toast? = null
 
     override fun init() {
-        setDrawerSideBar()
+        setBackPressedToast()
+        setBackPressed()
     }
 
-    private fun setDrawerSideBar() {
-        drawerLayout = binding.drawerLayoutMainSideBar
+    private fun setBackPressedToast() {
+        backPressedToast = Toast.makeText(
+            baseContext,
+            getString(R.string.main_back_pressed_message),
+            Toast.LENGTH_SHORT,
+        )
+    }
+
+    private fun setBackPressed() {
+        onBackPressedDispatcher.addCallback {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawers()
+            } else {
+                if (System.currentTimeMillis() - backPressedTime <= 2000L) {
+                    backPressedToast?.cancel()
+                    finish()
+                } else {
+                    backPressedTime = System.currentTimeMillis()
+                    backPressedToast?.show()
+                }
+            }
+        }
     }
 
     fun openDrawerButtonOnClick(view: View) {
