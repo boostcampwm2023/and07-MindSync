79296cf9c369b296f5f2abe6c1a86665823b4439
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-11-21 18:04:53 +0900

    design(#52): sidebar 기본 틀 완성

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/MainActivity.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/MainActivity.kt
@@ -1,12 +1,28 @@
 package boostcamp.and07.mindsync.ui
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import boostcamp.and07.mindsync.R
+import android.view.View
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import boostcamp.and07.mindsync.databinding.ActivityMainBinding
+import boostcamp.and07.mindsync.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity :
+    BaseActivity<ActivityMainBinding>(boostcamp.and07.mindsync.R.layout.activity_main) {
+    private lateinit var drawerLayout: DrawerLayout
+
+    override fun init() {
+        setDrawerSideBar()
+    }
+
+    private fun setDrawerSideBar() {
+        drawerLayout = binding.drawerLayoutMainSideBar
+    }
+
+    fun openDrawerButtonOnClick(view: View) {
+        drawerLayout.openDrawer(GravityCompat.START)
+    }
+
+    fun foldDrawerButtonOnClick(view: View) {
+        drawerLayout.closeDrawers()
     }
 }
