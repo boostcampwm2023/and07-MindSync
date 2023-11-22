4cfd580793c9b552e8e3067804703b23d74f981d
Conut-1 <1mim1@naver.com>
2023-11-22 23:46:32 +0900

    feat(#76): 논리적 시계 추가

---
+++ nestjs-BE/crdt/clock.ts
@@ -0,0 +1,33 @@
+export enum COMPARE {
+  GREATER,
+  LESS,
+}
+
+export class Clock {
+  id: string;
+  counter: number;
+
+  constructor(id: string, counter: number = 0) {
+    this.id = id;
+    this.counter = counter;
+  }
+
+  increment() {
+    this.counter++;
+  }
+
+  copy(): Clock {
+    return new Clock(this.id, this.counter);
+  }
+
+  merge(remoteClock: Clock): Clock {
+    return new Clock(this.id, Math.max(this.counter, remoteClock.counter));
+  }
+
+  compare(remoteClock: Clock): COMPARE {
+    if (this.counter > remoteClock.counter) return COMPARE.GREATER;
+    if (this.counter === remoteClock.counter && this.id > remoteClock.id)
+      return COMPARE.GREATER;
+    return COMPARE.LESS;
+  }
+}
