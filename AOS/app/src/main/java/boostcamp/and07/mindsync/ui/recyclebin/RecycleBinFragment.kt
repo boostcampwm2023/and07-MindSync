18f52c06dd82970adb5ad430e951a3b161911398
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-11-23 11:27:50 +0900

    design(#52): BoardListFragment, RecycleBinFragment 추가

---
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/recyclebin/RecycleBinFragment.kt
@@ -0,0 +1,10 @@
+package boostcamp.and07.mindsync.ui.recyclebin
+
+import boostcamp.and07.mindsync.R
+import boostcamp.and07.mindsync.databinding.FragmentRecycleBinBinding
+import boostcamp.and07.mindsync.ui.base.BaseFragment
+
+class RecycleBinFragment : BaseFragment<FragmentRecycleBinBinding>(R.layout.fragment_recycle_bin) {
+    override fun initView() {
+    }
+}
