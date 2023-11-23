8d41262e3183cb6a40f0f1b2d1e67d6c277660e3
tnpfldyd <dkdldhs0322@gmail.com>
2023-11-24 01:08:40 +0900

    fix(#58): 기능이 추가 됨에 따라 모듈 수정

--- nestjs-BE/cache-server/src/app.module.ts
+++ nestjs-BE/cache-server/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { TemporaryDatabaseModule } from './temporary-database/temporary-database.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ProfilesModule } from './profiles/profiles.module';
+import { SpacesModule } from './spaces/spaces.module';
 
 @Module({
-  imports: [TemporaryDatabaseModule],
+  imports: [
+    UsersModule,
+    PrismaModule,
+    TemporaryDatabaseModule,
+    ScheduleModule.forRoot(),
+    ProfilesModule,
+    SpacesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
