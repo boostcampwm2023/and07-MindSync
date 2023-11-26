3b6846a714d9bfa579a9e462c38b009c877d6355
Conut-1 <1mim1@naver.com>
2023-11-26 22:28:56 +0900

    delete: crdt map을 사용한 코드 제거

--- nestjs-BE/src/board/board.gateway.ts
+++ nestjs-BE/src/board/board.gateway.ts
@@ -4,33 +4,24 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { MindmapService } from '../mindmap/mindmap.service';
 
-interface BoardDataPayload {
+interface MindmapDataPayload {
   message: any;
   boardId: string;
 }
 
 @WebSocketGateway({ namespace: 'board' })
 export class BoardGateway {
-  constructor(private readonly mindmapService: MindmapService) {}
-
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('joinRoom')
-  handleJoinRoom(client: Socket, payload: { boardId: string }): void {
+  @SubscribeMessage('joinBoard')
+  handleJoinBoard(client: Socket, payload: { boardId: string }): void {
     client.join(payload.boardId);
   }
 
-  @SubscribeMessage('pushData')
-  handlePushNode(client: Socket, payload: BoardDataPayload): void {
-    this.mindmapService.updateMindmap(payload.boardId, payload.message);
-    client.broadcast
-      .to(payload.boardId)
-      .emit(
-        'messageFromServer',
-        this.mindmapService.getEncodedState(payload.boardId),
-      );
+  @SubscribeMessage('updateMindmap')
+  handleUpdateMindmap(client: Socket, payload: MindmapDataPayload) {
+    client.broadcast.to(payload.boardId).emit('stateFromServer', payload);
   }
 }
