b47678628d0a5d8b4c924b988d93b71d16202442
tnpfldyd <dkdldhs0322@gmail.com>
2023-11-28 18:58:22 +0900

    fix(#104): mongoDB client 생성 및 import 명 변경

--- nestjs-BE/cache-server/src/prisma/prisma.module.ts
+++ nestjs-BE/cache-server/src/prisma/prisma.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
+import { PrismaServiceMySQL } from './prisma.service';
+import { PrismaServiceMongoDB } from './prisma.service';
 
 @Global()
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [PrismaServiceMySQL, PrismaServiceMongoDB],
+  exports: [PrismaServiceMySQL, PrismaServiceMongoDB],
 })
 export class PrismaModule {}
