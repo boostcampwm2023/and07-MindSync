c7c3b67ad818b8823e8de39322669215d57673dd
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-11-28 11:33:26 +0900

    fix(#110): Children 순서를 일정하게 유지하기 위해 counter 값이 같으면 tree ID를 비교하도록 수정

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/crdt/Clock.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/crdt/Clock.kt
@@ -12,7 +12,8 @@ data class Clock(val id: String, var counter: Int = 0) : Comparable<Clock> {
     override fun compareTo(other: Clock): Int {
         return when {
             this.counter > other.counter -> 1
-            this.counter == other.counter -> 0
+            this.counter == other.counter && this.id > other.id -> 1
+            this.counter == other.counter && this.id == other.id -> 0
             else -> -1
         }
     }
