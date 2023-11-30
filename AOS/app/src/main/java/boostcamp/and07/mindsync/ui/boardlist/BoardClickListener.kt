cebb76c53ef32309728310c10d66286572008be8
jaehan4707 <jahan4707@naver.com>
2023-12-01 01:05:14 +0900

    feat(#123): BoardClickListener 생성

---
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/boardlist/BoardClickListener.kt
@@ -0,0 +1,9 @@
+package boostcamp.and07.mindsync.ui.boardlist
+
+import boostcamp.and07.mindsync.data.model.Board
+
+interface BoardClickListener {
+    fun onClick(board: Board)
+
+    fun onLongClick(board: Board)
+}
