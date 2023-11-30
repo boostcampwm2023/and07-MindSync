454fecb599dc85ea2406d292db5ce21fd6e12b71
jaehan4707 <jahan4707@naver.com>
2023-11-30 23:40:57 +0900

    feat(#123): data class Board 생성

---
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/model/Board.kt
@@ -0,0 +1,10 @@
+package boostcamp.and07.mindsync.data.model
+
+import java.time.LocalDate
+
+data class Board(
+    val id: String,
+    val name: String,
+    val date: LocalDate,
+    val imageUrl: String,
+)
