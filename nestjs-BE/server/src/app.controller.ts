8cf9cd2b1cea788f92eebbff51792ba1414e8d80
Conut-1 <1mim1@naver.com>
2023-12-02 19:32:31 +0900

    feat: 로그인 성공 테스트용 api 추가

--- nestjs-BE/server/src/app.controller.ts
+++ nestjs-BE/server/src/app.controller.ts
@@ -11,4 +11,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('login-test')
+  authTest(): string {
+    return 'login success';
+  }
 }
