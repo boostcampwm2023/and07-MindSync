6b68acb94748cdab0d3bed8cf5adbb1f33b06c60
jaehan4707 <jahan4707@naver.com>
2023-12-03 20:33:31 +0900

    feat(#130): fragment viewModel 연결 및 dataBinding 적용

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/AddSpaceFragment.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/AddSpaceFragment.kt
@@ -1,11 +1,18 @@
 package boostcamp.and07.mindsync.ui.space
 
+import androidx.fragment.app.viewModels
 import boostcamp.and07.mindsync.R
 import boostcamp.and07.mindsync.databinding.FragmentAddSpaceBinding
 import boostcamp.and07.mindsync.ui.base.BaseFragment
 
 class AddSpaceFragment : BaseFragment<FragmentAddSpaceBinding>(R.layout.fragment_add_space) {
+
+    private val addSpaceViewModel: AddSpaceViewModel by viewModels()
     override fun initView() {
-        TODO("Not yet implemented")
+        setBinding()
+    }
+
+    private fun setBinding() {
+        binding.vm = addSpaceViewModel
     }
 }
