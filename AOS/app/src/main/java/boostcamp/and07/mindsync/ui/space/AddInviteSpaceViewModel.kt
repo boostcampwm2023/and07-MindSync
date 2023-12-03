b5bd0725b1118a38c63c4daadafb1069e9f10c3d
jaehan4707 <jahan4707@naver.com>
2023-12-03 22:17:19 +0900

    feat(#130): AddInviteSpaceViewModel 구현

---
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/AddInviteSpaceViewModel.kt
@@ -0,0 +1,14 @@
+package boostcamp.and07.mindsync.ui.space
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class AddInviteSpaceViewModel : ViewModel() {
+    private val _spaceInviteCode = MutableStateFlow("")
+    val spaceInviteCode: StateFlow<String> = _spaceInviteCode
+
+    fun onSpaceInviteCodeChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+        _spaceInviteCode.value = s.toString()
+    }
+}
