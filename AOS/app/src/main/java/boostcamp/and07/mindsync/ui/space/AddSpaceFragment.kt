c4655bed4b024224c3665beb25f3bd13863a3dd7
jaehan4707 <jahan4707@naver.com>
2023-12-03 19:21:53 +0900

    design(#130): 새 스페이스 만들기 UI 구현

---
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/AddSpaceFragment.kt
@@ -0,0 +1,11 @@
+package boostcamp.and07.mindsync.ui.space
+
+import boostcamp.and07.mindsync.R
+import boostcamp.and07.mindsync.databinding.FragmentAddSpaceBinding
+import boostcamp.and07.mindsync.ui.base.BaseFragment
+
+class AddSpaceFragment : BaseFragment<FragmentAddSpaceBinding>(R.layout.fragment_add_space) {
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+}
