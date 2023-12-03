ad4920b24c3b1aa13285822b03282bad686b449e
jaehan4707 <jahan4707@naver.com>
2023-12-03 20:53:04 +0900

    feat(#130): 이미지 버튼 클릭 -> Dialog 출력 구현

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/AddSpaceFragment.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/AddSpaceFragment.kt
@@ -1,12 +1,13 @@
 package boostcamp.and07.mindsync.ui.space
 
+import android.util.Log
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import boostcamp.and07.mindsync.R
 import boostcamp.and07.mindsync.databinding.FragmentAddSpaceBinding
 import boostcamp.and07.mindsync.ui.base.BaseFragment
 
 class AddSpaceFragment : BaseFragment<FragmentAddSpaceBinding>(R.layout.fragment_add_space) {
-
     private val addSpaceViewModel: AddSpaceViewModel by viewModels()
     override fun initView() {
         setBinding()
@@ -14,5 +15,10 @@ class AddSpaceFragment : BaseFragment<FragmentAddSpaceBinding>(R.layout.fragment
 
     private fun setBinding() {
         binding.vm = addSpaceViewModel
+        binding.view = this
+    }
+
+    fun clickImageButton() {
+        findNavController().navigate(R.id.action_to_chooseSpaceImageDialog)
     }
 }
