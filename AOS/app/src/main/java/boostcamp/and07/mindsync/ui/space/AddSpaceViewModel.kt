31c2c0f541408774718ce97a34563f3d338a2da9
jaehan4707 <jahan4707@naver.com>
2023-12-03 21:41:51 +0900

    feat(#130): AddSpaceViewModel Thumbnail 추가

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/AddSpaceViewModel.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/AddSpaceViewModel.kt
@@ -7,8 +7,13 @@ import kotlinx.coroutines.flow.StateFlow
 class AddSpaceViewModel : ViewModel() {
     private val _spaceName = MutableStateFlow<String>("")
     val spaceName: StateFlow<String> = _spaceName
-
+    private val _spaceThumbnail = MutableStateFlow<String>("")
+    val spaceThumbnail: StateFlow<String> = _spaceThumbnail
     fun onSpaceNameChanged(s: CharSequence, start: Int, before: Int, count: Int) {
         _spaceName.value = s.toString()
+    }
+
+    fun setSpaceThumbnail(thumbnailUrl: String) {
+        _spaceThumbnail.value = thumbnailUrl
     }
 }
