77acf9f25f1237b8d48d57754fa62f0a18ecea55
jaehan4707 <jahan4707@naver.com>
2023-12-03 22:16:45 +0900

    design(#130): AddInviteSpaceFragment UI 구현

---
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/AddInviteSpaceFragment.kt
@@ -0,0 +1,11 @@
+package boostcamp.and07.mindsync.ui.space
+
+import boostcamp.and07.mindsync.R
+import boostcamp.and07.mindsync.databinding.FragmentAddInviteSpaceBinding
+import boostcamp.and07.mindsync.ui.base.BaseFragment
+
+class AddInviteSpaceFragment :
+    BaseFragment<FragmentAddInviteSpaceBinding>(R.layout.fragment_add_invite_space) {
+    override fun initView() {
+    }
+}
