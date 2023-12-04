f37879fb24b77aef497f69f3a0b14fb9bae8282c
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-12-04 19:00:57 +0900

    design(#94): 로그인 Activity 추가

---
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/login/LoginActivity.kt
@@ -0,0 +1,10 @@
+package boostcamp.and07.mindsync.ui.login
+
+import boostcamp.and07.mindsync.R
+import boostcamp.and07.mindsync.databinding.ActivityLoginBinding
+import boostcamp.and07.mindsync.ui.base.BaseActivity
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    override fun init() {
+    }
+}
