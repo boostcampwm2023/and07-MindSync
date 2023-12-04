b169ef0e177ebfe54817e424ba7e2f82a17c32e1
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-12-04 23:43:50 +0900

    refactor(#93): 불필요한 주석 제거

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/login/LoginRepository.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/login/LoginRepository.kt
@@ -2,6 +2,4 @@ package boostcamp.and07.mindsync.data.repository.login
 
 interface LoginRepository {
     suspend fun loginWithKakao(kakaoUserId: String): Result<String>
-
-//    fun loginWithGoogle()
 }
