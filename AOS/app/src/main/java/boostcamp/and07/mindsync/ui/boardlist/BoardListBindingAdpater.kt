151afa8c89716cd11b7e5c3067a355a891e7471b
jaehan4707 <jahan4707@naver.com>
2023-12-04 11:43:38 +0900

    feat(#130): flexBox Content center로 수정

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/boardlist/BoardListBindingAdpater.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/boardlist/BoardListBindingAdpater.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import boostcamp.and07.mindsync.R
 import boostcamp.and07.mindsync.data.model.Board
 import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
@@ -28,6 +29,7 @@ fun RecyclerView.bindLayoutManager(direction: Int) {
     this.layoutManager =
         FlexboxLayoutManager(context).apply {
             flexDirection = direction
+            justifyContent = JustifyContent.CENTER
         }
 }
 
