00452ac58afddbeb2e7d8b58c652f00116c161dd
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-12-04 21:55:11 +0900

    feat(#93): Kakao Login Request data, Login Response data 추가

---
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/response/LoginResponse.kt
@@ -0,0 +1,10 @@
+package boostcamp.and07.mindsync.data.network.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LoginResponse(
+    @SerialName("access_token")
+    val accessToken: String,
+)
