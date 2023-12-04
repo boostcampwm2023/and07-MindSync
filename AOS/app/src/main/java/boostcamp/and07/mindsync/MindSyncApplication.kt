a629870c8dd8233b57c7f244577dbdc6b365e190
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-12-04 21:32:45 +0900

    fix(#95): 중복 Application 제거 및 톷합

--- AOS/app/src/main/java/boostcamp/and07/mindsync/MindSyncApplication.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/MindSyncApplication.kt
@@ -2,7 +2,9 @@ package boostcamp.and07.mindsync
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MindSyncApplication : Application() {
     override fun onCreate() {
         super.onCreate()
--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/MindSyncApplication.kt
+++
@@ -1,7 +0,0 @@
-package boostcamp.and07.mindsync.ui
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MindSyncApplication : Application()
