965fa274decc789bc3a951d8dd3493d59afecb7c
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-12-04 19:34:49 +0900

    feat(#95): kakao login 기능 추가

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/login/LoginActivity.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/login/LoginActivity.kt
@@ -1,10 +1,93 @@
 package boostcamp.and07.mindsync.ui.login
 
+import android.content.Intent
+import android.util.Log
+import android.view.View
 import boostcamp.and07.mindsync.R
 import boostcamp.and07.mindsync.databinding.ActivityLoginBinding
 import boostcamp.and07.mindsync.ui.base.BaseActivity
+import boostcamp.and07.mindsync.ui.main.MainActivity
+import com.kakao.sdk.auth.AuthApiClient
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
-    override fun init() {
+    override fun init() {}
+
+    override fun onStart() {
+        super.onStart()
+        checkKakaoSignIn()
+    }
+
+    private fun checkKakaoSignIn() {
+        if (AuthApiClient.instance.hasToken()) {
+            UserApiClient.instance.accessTokenInfo { _, error ->
+                if (error == null) {
+                    successKakaoLoin()
+                }
+            }
+        }
+    }
+
+    fun kakaoSignInOnClick(view: View) {
+        val resultCallBack: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                Log.e("LoginActivity", "kakao 로그인 실패", error)
+            } else if (token != null) {
+                Log.i("LoginActivity", "kakao 로그인 성공 ${token.accessToken}")
+                successKakaoLoin()
+            }
+        }
+
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                if (error != null) {
+                    Log.e("LoginActivity", "카카오톡으로 로그인 실패", error)
+
+                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+
+                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                    UserApiClient.instance.loginWithKakaoAccount(
+                        this,
+                        callback = resultCallBack,
+                    )
+                } else if (token != null) {
+                    Log.i("LoginActivity", "kakao 로그인 성공 ${token.accessToken}")
+                    successKakaoLoin()
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(this, callback = resultCallBack)
+        }
+    }
+
+    private fun successKakaoLoin() {
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                Log.e("LoginActivity", "사용자 정보 요청 실패", error)
+            } else if (user != null) {
+                Log.i(
+                    "LoginActivity",
+                    "사용자 정보 요청 성공" +
+                        "\n회원번호: ${user.id}" +
+                        "\n이메일: ${user.kakaoAccount?.email}" +
+                        "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+                        "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}",
+                )
+            }
+        }
+        startMainActivity()
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
