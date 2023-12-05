cabf53c93eb76221a7594e23f2ba493b0eb0983d
jaehan4707 <jahan4707@naver.com>
2023-12-05 11:15:27 +0900

    refactor(#157): 갤러리 API 32이하 버전 대응

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/AddSpaceActivity.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/AddSpaceActivity.kt
@@ -1,8 +1,16 @@
 package boostcamp.and07.mindsync.ui.space
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
 import boostcamp.and07.mindsync.R
 import boostcamp.and07.mindsync.databinding.ActivityAddSpaceBinding
 import boostcamp.and07.mindsync.ui.base.BaseActivity
@@ -19,6 +27,22 @@ class AddSpaceActivity : BaseActivity<ActivityAddSpaceBinding>(R.layout.activity
                 addSpaceViewModel.setSpaceThumbnail(url.toString())
             }
         }
+    private val galleryPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                launchImagePicker()
+            }
+        }
+
+    private val imageResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            result.data?.data?.let { uri ->
+                createImage(uri)
+            }
+        }
+    }
 
     override fun init() {
         setBinding()
@@ -30,6 +54,92 @@ class AddSpaceActivity : BaseActivity<ActivityAddSpaceBinding>(R.layout.activity
     }
 
     fun clickImageButton() {
+        checkPermissionsAndLaunchImagePicker()
+    }
+
+    private fun createImage(uri: Uri?) {
+        uri?.let { uri ->
+            addSpaceViewModel.setSpaceThumbnail(uri.toString())
+        }
+    }
+
+    private fun checkPermissionsAndLaunchImagePicker() {
+        when {
+            checkSelfPermission(
+                this,
+                getPermissionReadMediaImagesOrReadExternalStorage(),
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                launchImagePicker()
+            }
+
+            shouldShowRequestPermissionRationale(
+                getPermissionReadMediaImagesOrReadExternalStorage(),
+            ) -> {
+                requestGalleryPermission()
+            }
+
+            else -> {
+                requestGalleryPermission()
+            }
+        }
+    }
+
+    private fun launchImagePicker() {
         pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private fun getPermissionReadMediaImagesOrReadExternalStorage() =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            Manifest.permission.READ_MEDIA_IMAGES
+        } else {
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        }
+
+    private fun requestGalleryPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            galleryPermissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+        } else {
+            checkPermissionAndLaunchImageSelector()
+        }
+    }
+
+    private fun checkPermissionAndLaunchImageSelector() {
+        val readPermission = checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        if (readPermission == PackageManager.PERMISSION_GRANTED) {
+            launchImageSelector()
+        } else {
+            requestExternalStoragePermission()
+        }
+    }
+
+    private fun requestExternalStoragePermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            REQUEST_CODE_PERMISSIONS,
+        )
+    }
+
+    private fun launchImageSelector() {
+        val intent = Intent(Intent.ACTION_PICK).apply {
+            setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+        }
+        imageResult.launch(intent)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            launchImagePicker()
+        }
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 1
     }
 }
