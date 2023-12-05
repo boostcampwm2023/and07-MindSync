a338e2dfe2dd118985c0c7badd9fd99c4ed493e1
tnpfldyd <dkdldhs0322@gmail.com>
2023-12-05 23:23:01 +0900

    fix(#133): access token payload 이메일 삭제됨에 따라 변경

--- nestjs-BE/server/src/auth/jwt.strategy.ts
+++ nestjs-BE/server/src/auth/jwt.strategy.ts
@@ -14,6 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { uuid: payload.sub, email: payload.email };
+    return { uuid: payload.sub };
   }
 }
