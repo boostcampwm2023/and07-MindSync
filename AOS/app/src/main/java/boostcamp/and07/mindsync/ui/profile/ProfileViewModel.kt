8fc14eb25687876544cba86cb5b25d3784260f44
dltkd1395 <dltkd13956@naver.com>
2023-12-06 00:55:44 +0900

    feat(#171): ProfileImageFile 설정 및 ProfileFileImage 추가

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/profile/ProfileViewModel.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/profile/ProfileViewModel.kt
@@ -2,13 +2,18 @@ package boostcamp.and07.mindsync.ui.profile
 
 import android.net.Uri
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import boostcamp.and07.mindsync.data.repository.profile.ProfileRepository
+import boostcamp.and07.mindsync.ui.util.fileToMultiPart
+import boostcamp.and07.mindsync.ui.util.toRequestBody
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
@@ -32,6 +37,28 @@ class ProfileViewModel
         fun updateNickName(nickname: String) {
             _uiState.update { uiState ->
                 uiState.copy(nickname = nickname)
+            }
+        }
+
+        fun setProfileImageFile(file: File) {
+            _uiState.update { uiState ->
+                uiState.copy(imageFile = file)
+            }
+        }
+
+        fun addProfile(imageName: String) {
+            _uiState.value.imageFile?.let { file ->
+                val icon = fileToMultiPart(file, imageName)
+                val nickname = _uiState.value.nickname.toRequestBody()
+                viewModelScope.launch {
+                    profileRepository.addProfile(nickname, icon)
+                        .onSuccess {
+                            _event.emit(ProfileUiEvent.NavigateToBack)
+                        }
+                        .onFailure {
+                            _event.emit(ProfileUiEvent.ShowMessage(it.message.toString()))
+                        }
+                }
             }
         }
     }
