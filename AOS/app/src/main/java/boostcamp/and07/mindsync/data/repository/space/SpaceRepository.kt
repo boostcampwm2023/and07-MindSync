b48358174b71c0813e9c68d83a1303fcb446e626
jaehan4707 <jahan4707@naver.com>
2023-12-05 12:26:21 +0900

    feat(#150): SpaceRepositoryImpl, SpaceRepository addSpace 추가

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/space/SpaceRepository.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/space/SpaceRepository.kt
@@ -1,3 +1,11 @@
 package boostcamp.and07.mindsync.data.repository.space
 
-interface SpaceRepository
+import boostcamp.and07.mindsync.data.model.Space
+import okhttp3.MultipartBody
+
+interface SpaceRepository {
+    suspend fun addSpace(
+        name: String,
+        icon: MultipartBody.Part,
+    ): Result<Space>
+}
--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/space/SpaceRepositoryImpl.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/space/SpaceRepositoryImpl.kt
@@ -1,7 +1,35 @@
 package boostcamp.and07.mindsync.data.repository.space
 
+import boostcamp.and07.mindsync.data.model.Space
+import boostcamp.and07.mindsync.data.network.SpaceApi
+import okhttp3.MultipartBody
 import javax.inject.Inject
 
 class SpaceRepositoryImpl
-    @Inject
-    constructor() : SpaceRepository
+@Inject
+constructor(private val spaceApi: SpaceApi) : SpaceRepository {
+    override suspend fun addSpace(
+        name: String,
+        icon: MultipartBody.Part,
+    ): Result<Space> {
+        val response = spaceApi.addSpace(name, icon)
+        return try {
+            if (response.isSuccessful) {
+                response.body()?.let { spaceResponse ->
+                    return Result.success(
+                        Space(
+                            id = spaceResponse.uuid,
+                            name = spaceResponse.name ?: "",
+                            imageUrl = spaceResponse.icon ?: "",
+                        ),
+                    )
+                }
+                throw Exception("body null")
+            } else {
+                Result.failure(Exception("response fail"))
+            }
+        } catch (e: Exception) {
+            Result.failure(Exception(e.message))
+        }
+    }
+}
