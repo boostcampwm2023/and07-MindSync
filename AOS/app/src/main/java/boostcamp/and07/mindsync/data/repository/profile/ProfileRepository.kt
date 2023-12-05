354299c4268fb4fbdde548ea8c436adb223c8634
dltkd1395 <dltkd13956@naver.com>
2023-12-05 19:16:48 +0900

    feat(#171): Repository addProfile 추가

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/profile/ProfileRepository.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/profile/ProfileRepository.kt
@@ -1,3 +1,12 @@
 package boostcamp.and07.mindsync.data.repository.profile
 
-interface ProfileRepository
+import boostcamp.and07.mindsync.data.model.Profile
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+
+interface ProfileRepository {
+    suspend fun addProfile(
+        nickname: RequestBody,
+        image: MultipartBody.Part,
+    ): Result<Profile>
+}
--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/profile/ProfileRepositoryImpl.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/profile/ProfileRepositoryImpl.kt
@@ -1,7 +1,36 @@
 package boostcamp.and07.mindsync.data.repository.profile
 
+import boostcamp.and07.mindsync.data.model.Profile
+import boostcamp.and07.mindsync.data.network.ProfileApi
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import javax.inject.Inject
 
 class ProfileRepositoryImpl
-    @Inject
-    constructor() : ProfileRepository
+@Inject
+constructor(private val profileApi: ProfileApi) : ProfileRepository {
+    override suspend fun addProfile(
+        nickname: RequestBody,
+        image: MultipartBody.Part,
+    ): Result<Profile> {
+        val response = profileApi.addProfile(nickname, image)
+        return try {
+            if (response.isSuccessful) {
+                response.body()?.let { profileResponse ->
+                    return Result.success(
+                        Profile(
+                            id = profileResponse.uuid,
+                            imageUrl = profileResponse.image ?: "",
+                            nickname = profileResponse.nickname ?: "",
+                        ),
+                    )
+                }
+                throw Exception()
+            } else {
+                Result.failure(Exception())
+            }
+        } catch (e: Exception) {
+            Result.failure(Exception())
+        }
+    }
+}
