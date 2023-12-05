cda9c3506e2a4bf93ee3a4c4009e307aea391b55
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-12-05 21:37:32 +0900

    fix(#161): Hilt cycle 문제를 Retrofit 두개 받는 걸로 해결

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/di/NetworkModule.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/di/NetworkModule.kt
@@ -1,10 +1,8 @@
 package boostcamp.and07.mindsync.data.di
 
 import boostcamp.and07.mindsync.BuildConfig
-import boostcamp.and07.mindsync.data.network.TokenApi
 import boostcamp.and07.mindsync.data.repository.login.AccessTokenInterceptor
 import boostcamp.and07.mindsync.data.repository.login.TokenAuthenticator
-import boostcamp.and07.mindsync.data.repository.login.TokenRepository
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
@@ -14,26 +12,12 @@ import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
-    @Singleton
-    @Provides
-    fun provideAccessTokenInterceptor(tokenRepository: TokenRepository): AccessTokenInterceptor {
-        return AccessTokenInterceptor(tokenRepository)
-    }
-
-    @Singleton
-    @Provides
-    fun provideTokenAuthenticator(
-        tokenRepository: TokenRepository,
-        tokenApi: TokenApi,
-    ): TokenAuthenticator {
-        return TokenAuthenticator(tokenRepository, tokenApi)
-    }
-
     @Singleton
     @Provides
     fun provideOkHttpClient(
@@ -48,9 +32,20 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    @Named(NetworkConst.TOKEN_RETROFIT)
+    fun provideSimpleRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .addConverterFactory(Json.asConverterFactory(NetworkConst.APPLICATION_JSON.toMediaType()))
+            .baseUrl(BuildConfig.BASE_URL)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    @Named(NetworkConst.CLIENT_RETROFIT)
+    fun provideInterceptorRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(Json.asConverterFactory(NetworkConst.APPLICATION_JSON.toMediaType()))
             .client(okHttpClient)
             .baseUrl(BuildConfig.BASE_URL)
             .build()
