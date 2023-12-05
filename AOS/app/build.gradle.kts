3e1061f3111eb3bb933cb693a0a0f580fb22115d
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-12-05 15:10:23 +0900

    chore(#160): DataStore 라이브러리 추가

--- AOS/app/build.gradle.kts
+++ AOS/app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
     // test
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
