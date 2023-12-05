f22924fe8fef5bf975a0af1151a26af8a105e10f
jaehan4707 <jahan4707@naver.com>
2023-12-05 16:37:28 +0900

    style(#150): ktlintFormat 적용

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/util/ExceptionMessage.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/util/ExceptionMessage.kt
@@ -15,6 +15,6 @@ enum class NetworkExceptionMessage(val message: String) {
     ERROR_MESSAGE_CANT_GET_TOKEN("Can't get server access token"),
 }
 
-enum class SpaceExceptionMessage(val message: String){
-    ERROR_MESSAGE_SPACE_ADD("Space Add failed")
+enum class SpaceExceptionMessage(val message: String) {
+    ERROR_MESSAGE_SPACE_ADD("Space Add failed"),
 }
