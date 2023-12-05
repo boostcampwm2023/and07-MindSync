71624c8757839a197d6d7abf5120017a3e32a9cc
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-12-06 00:37:22 +0900

    style: ktlint 적용

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/login/TokenRepositoryImpl.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/login/TokenRepositoryImpl.kt
@@ -12,55 +12,55 @@ import java.io.IOException
 import javax.inject.Inject
 
 class TokenRepositoryImpl
-@Inject
-constructor(
-    private val dataStore: DataStore<Preferences>,
-) : TokenRepository {
-    override fun getAccessToken(): Flow<String?> {
-        return dataStore.data.catch { exception ->
-            if (exception is IOException) {
-                exception.printStackTrace()
-                emit(emptyPreferences())
-            } else {
-                throw exception
+    @Inject
+    constructor(
+        private val dataStore: DataStore<Preferences>,
+    ) : TokenRepository {
+        override fun getAccessToken(): Flow<String?> {
+            return dataStore.data.catch { exception ->
+                if (exception is IOException) {
+                    exception.printStackTrace()
+                    emit(emptyPreferences())
+                } else {
+                    throw exception
+                }
+            }.map { prefs ->
+                prefs[ACCESS_TOKEN_KEY]
             }
-        }.map { prefs ->
-            prefs[ACCESS_TOKEN_KEY]
+        }
+
+        override fun getRefreshToken(): Flow<String?> {
+            return dataStore.data.map { prefs ->
+                prefs[REFRESH_TOKEN_KEY]
+            }
+        }
+
+        override suspend fun saveAccessToken(token: String) {
+            dataStore.edit { prefs ->
+                prefs[ACCESS_TOKEN_KEY] = token
+            }
+        }
+
+        override suspend fun saveRefreshToken(token: String) {
+            dataStore.edit { prefs ->
+                prefs[REFRESH_TOKEN_KEY] = token
+            }
+        }
+
+        override suspend fun deleteAccessToken() {
+            dataStore.edit { prefs ->
+                prefs.remove(ACCESS_TOKEN_KEY)
+            }
+        }
+
+        override suspend fun deleteRefreshToken() {
+            dataStore.edit { prefs ->
+                prefs.remove(REFRESH_TOKEN_KEY)
+            }
+        }
+
+        companion object {
+            private val ACCESS_TOKEN_KEY = stringPreferencesKey(DataStoreConst.ACCESS_TOKEN_KEY_NAME)
+            private val REFRESH_TOKEN_KEY = stringPreferencesKey(DataStoreConst.REFRESH_TOKEN_KEY_NAME)
         }
     }
-
-    override fun getRefreshToken(): Flow<String?> {
-        return dataStore.data.map { prefs ->
-            prefs[REFRESH_TOKEN_KEY]
-        }
-    }
-
-    override suspend fun saveAccessToken(token: String) {
-        dataStore.edit { prefs ->
-            prefs[ACCESS_TOKEN_KEY] = token
-        }
-    }
-
-    override suspend fun saveRefreshToken(token: String) {
-        dataStore.edit { prefs ->
-            prefs[REFRESH_TOKEN_KEY] = token
-        }
-    }
-
-    override suspend fun deleteAccessToken() {
-        dataStore.edit { prefs ->
-            prefs.remove(ACCESS_TOKEN_KEY)
-        }
-    }
-
-    override suspend fun deleteRefreshToken() {
-        dataStore.edit { prefs ->
-            prefs.remove(REFRESH_TOKEN_KEY)
-        }
-    }
-
-    companion object {
-        private val ACCESS_TOKEN_KEY = stringPreferencesKey(DataStoreConst.ACCESS_TOKEN_KEY_NAME)
-        private val REFRESH_TOKEN_KEY = stringPreferencesKey(DataStoreConst.REFRESH_TOKEN_KEY_NAME)
-    }
-}
