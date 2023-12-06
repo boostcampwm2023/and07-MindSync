94e28b6b6e8ee7b927baa173f189de30ce495c64
jaehan4707 <jahan4707@naver.com>
2023-12-06 23:35:15 +0900

    feat(#196): getInviteCode 적용

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/dialog/InviteUserDialog.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/dialog/InviteUserDialog.kt
@@ -9,13 +9,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.lifecycleScope
 import boostcamp.and07.mindsync.R
+import boostcamp.and07.mindsync.data.repository.space.SpaceRepository
 import boostcamp.and07.mindsync.databinding.DialogInviteUserBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class InviteUserDialog : DialogFragment() {
     private var _binding: DialogInviteUserBinding? = null
     private val binding get() = _binding!!
+
+    @Inject
+    lateinit var spaceRepository: SpaceRepository
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog = super.onCreateDialog(savedInstanceState)
@@ -33,12 +43,24 @@ class InviteUserDialog : DialogFragment() {
         _binding = DialogInviteUserBinding.inflate(inflater, container, false)
         binding.run {
         }
+        getInviteCode()
         return binding.root
     }
 
     override fun onStart() {
         super.onStart()
         resizeDialog()
+    }
+
+    private fun getInviteCode() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            spaceRepository.getInviteSpaceCode("11ee9430df67b1c086cf137f14539c56").onSuccess { inviteCode ->
+                binding.tvInviteUserSpaceCode.text = inviteCode
+                Toast.makeText(requireContext(), "성공!", Toast.LENGTH_SHORT).show()
+            }.onFailure {
+                Toast.makeText(requireContext(), "실패!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun resizeDialog() {
