dde3b2edb950725af6cec1afaf47dd4a9d638d16
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-12-06 17:23:27 +0900

    refactor(#174): BaseActivity에서 LogoutEventRepsoitory collect 제거
    추후 BaseViewModel을 이용해 추가할 예정

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/base/BaseActivity.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/base/BaseActivity.kt
@@ -1,27 +1,14 @@
 package boostcamp.and07.mindsync.ui.base
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import boostcamp.and07.mindsync.data.repository.login.LogoutEvent
-import boostcamp.and07.mindsync.data.repository.login.LogoutEventRepository
-import boostcamp.and07.mindsync.ui.login.LoginActivity
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 abstract class BaseActivity<T : ViewDataBinding>(private val layoutResId: Int) :
     AppCompatActivity() {
     private var _binding: T? = null
     val binding get() = _binding!!
-
-    @Inject
-    lateinit var logoutRepository: LogoutEventRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,29 +19,9 @@ abstract class BaseActivity<T : ViewDataBinding>(private val layoutResId: Int) :
 
     abstract fun init()
 
-    open fun setLogoutObserve() {
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                logoutRepository.logoutEvent.collectLatest { event ->
-                    when (event) {
-                        is LogoutEvent.Logout -> {
-                            navigateToLoginScreen()
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
 
-    private fun navigateToLoginScreen() {
-        val intent = Intent(this, LoginActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
-        startActivity(intent)
-        finish()
-    }
 }
