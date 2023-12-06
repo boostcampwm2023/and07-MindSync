41c636d90630376cf41b0a69643b229b927f7281
dltkd1395 <dltkd13956@naver.com>
2023-12-07 03:13:29 +0900

    refactor(#171): POST관련 함수 제거 및 프로필 API 응답 타입을 ProfileDTO로 변경

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/ProfileApi.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/ProfileApi.kt
@@ -1,26 +1,21 @@
 package boostcamp.and07.mindsync.data.network
 
-import boostcamp.and07.mindsync.data.network.response.ProfileResponse
+import boostcamp.and07.mindsync.data.network.response.ProfileDTO
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Multipart
-import retrofit2.http.POST
+import retrofit2.http.PATCH
 import retrofit2.http.Part
-import retrofit2.http.Path
 
 interface ProfileApi {
-    @POST("profiles")
-    @Multipart
-    suspend fun addProfile(
-        @Part("nickname")
-        nickname: RequestBody,
-        @Part image: MultipartBody.Part?,
-    ): Response<ProfileResponse>
+    @GET("profiles")
+    suspend fun getProfile(): ProfileDTO
 
-    @GET("profiles/{profile_uuid}")
-    suspend fun getProfile(
-        @Path("profile_uuid") uuid: String = "11ee9388fa933210a70e3f107ae3ec5d",
-    ): Response<ProfileResponse>
+    @PATCH("profiles")
+    @Multipart
+    suspend fun patchProfile(
+        @Part("nickname") nickname: RequestBody,
+        @Part image: MultipartBody.Part?,
+    ): ProfileDTO
 }
