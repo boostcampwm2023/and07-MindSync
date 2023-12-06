afca6efb7c7bbb25b7d654c32ad4fbfdb6b573d9
Conut-1 <1mim1@naver.com>
2023-12-06 19:08:32 +0900

    feat(#19, #106): 보드 입장 시 마인드맵 트리 데이터 보내기

--- nestjs-BE/server/src/board-trees/board-trees.module.ts
+++ nestjs-BE/server/src/board-trees/board-trees.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { BoardTreesService } from './board-trees.service';
 import { BoardTreesGateway } from './board-trees.gateway';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BoardTree, BoardTreeSchema } from './schemas/board-tree.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: BoardTree.name, schema: BoardTreeSchema },
+    ]),
+  ],
   providers: [BoardTreesService, BoardTreesGateway],
 })
 export class BoardTreesModule {}
--- nestjs-BE/server/src/board-trees/board-trees.service.ts
+++ nestjs-BE/server/src/board-trees/board-trees.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { BoardTree } from './schemas/board-tree.schema';
+import { Model } from 'mongoose';
+import { CrdtTree } from 'src/crdt/crdt-tree';
 
 @Injectable()
-export class BoardTreesService {}
+export class BoardTreesService {
+  constructor(
+    @InjectModel(BoardTree.name) private boardTreeModel: Model<BoardTree>,
+  ) {}
+
+  private boardTrees = new Map<string, CrdtTree<string>>();
+
+  async create(boardId: string, tree: string) {
+    const createdTree = new this.boardTreeModel({
+      boardId,
+      tree,
+    });
+    return createdTree.save();
+  }
+
+  async findByBoardId(boardId: string) {
+    return this.boardTreeModel.findOne({ boardId }).exec();
+  }
+
+  getTreeData(boardId: string) {
+    const boardTree = this.boardTrees.get(boardId);
+    return JSON.stringify(boardTree);
+  }
+
+  async initBoardTree(boardId: string) {
+    const existingTree = await this.findByBoardId(boardId);
+    if (existingTree) {
+      this.boardTrees.set(boardId, CrdtTree.parse<string>(existingTree.tree));
+    } else {
+      const newTree = new CrdtTree<string>(boardId);
+      this.create(boardId, JSON.stringify(newTree));
+      this.boardTrees.set(boardId, newTree);
+    }
+  }
+
+  hasTree(boardId: string) {
+    return this.boardTrees.has(boardId);
+  }
+}
