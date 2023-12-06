b62df110c85c0319bda1616d60fed853538e4ef0
tnpfldyd <dkdldhs0322@gmail.com>
2023-12-07 06:06:36 +0900

    fix(#188): 스페이스 만든 사람은 자동으로 참여되도록 변경

--- nestjs-BE/server/src/spaces/spaces.module.ts
+++ nestjs-BE/server/src/spaces/spaces.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { SpacesService } from './spaces.service';
 import { SpacesController } from './spaces.controller';
 import { UploadService } from 'src/upload/upload.service';
+import { ProfileSpaceModule } from 'src/profile-space/profile-space.module';
 
 @Module({
+  imports: [forwardRef(() => ProfileSpaceModule)],
   controllers: [SpacesController],
   providers: [SpacesService, UploadService],
+  exports: [SpacesService],
 })
 export class SpacesModule {}
