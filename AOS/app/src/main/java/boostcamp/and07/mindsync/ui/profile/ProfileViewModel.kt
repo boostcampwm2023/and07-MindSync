90bc15213e00a1fdff2ef32544e9e63ba9cf202c
dltkd1395 <dltkd13956@naver.com>
2023-12-07 03:20:08 +0900

    feat(#171): 프로필 업데이트 및 프로필 정보 추출 구현

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/profile/ProfileViewModel.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/profile/ProfileViewModel.kt
@@ -7,10 +7,12 @@ import boostcamp.and07.mindsync.data.repository.profile.ProfileRepository
 import boostcamp.and07.mindsync.ui.util.fileToMultiPart
 import boostcamp.and07.mindsync.ui.util.toRequestBody
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.io.File
@@ -18,15 +20,11 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel
-    @Inject
-    constructor(
-        private val profileRepository: ProfileRepository,
-    ) :
+@Inject
+constructor(
+    private val profileRepository: ProfileRepository,
+) :
     ViewModel() {
-        private val _uiState = MutableStateFlow(ProfileUiState())
-        val uiState: StateFlow<ProfileUiState> = _uiState
-        private val _event = MutableSharedFlow<ProfileUiEvent>()
-        val event: SharedFlow<ProfileUiEvent> = _event
     private val _uiState = MutableStateFlow(ProfileUiState())
     val uiState: StateFlow<ProfileUiState> = _uiState
     private val _event = MutableSharedFlow<ProfileUiEvent>()
@@ -36,25 +34,46 @@ class ProfileViewModel
             viewModelScope.launch { _event.emit(ProfileUiEvent.ShowMessage(throwable.message.toString())) }
         }
 
-        fun updateProfileUri(uri: Uri) {
-            _uiState.update { uiState ->
-                uiState.copy(imageUri = uri)
-            }
+    fun updateProfileUri(uri: Uri) {
+        _uiState.update { uiState ->
+            uiState.copy(imageUri = uri)
         }
+    }
 
-        fun updateNickName(nickname: String) {
-            _uiState.update { uiState ->
-                uiState.copy(nickname = nickname)
-            }
+    fun updateNickName(nickname: String) {
+        _uiState.update { uiState ->
+            uiState.copy(nickname = nickname)
         }
+    }
 
-        fun setProfileImageFile(file: File) {
-            _uiState.update { uiState ->
-                uiState.copy(imageFile = file)
-            }
+    fun setProfileImageFile(file: File) {
+        _uiState.update { uiState ->
+            uiState.copy(imageFile = file)
         }
+    }
 
+    fun updateProfile(imageName: String) {
+        _uiState.value.imageFile?.let { file ->
+            val image = fileToMultiPart(file, imageName)
+            val nickname = _uiState.value.nickname.toRequestBody()
+            viewModelScope.launch(coroutineExceptionHandler) {
+                profileRepository.patchProfile(nickname, image).collectLatest {
+                    _event.emit(ProfileUiEvent.NavigateToBack)
                 }
             }
         }
     }
+
+    fun fetchProfile() {
+        viewModelScope.launch(coroutineExceptionHandler) {
+            profileRepository.getProfile().collectLatest { profile ->
+                _uiState.update { uiState ->
+                    uiState.copy(
+                        nickname = profile.nickname,
+                        imageUri = Uri.parse(profile.imageUrl),
+                    )
+                }
+            }
+        }
+    }
+}
