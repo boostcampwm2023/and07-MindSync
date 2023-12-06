34005d4564bb152a57eecd73a7f7fbfb3ee84d9e
jaehan4707 <jahan4707@naver.com>
2023-12-06 23:33:26 +0900

    feat(#196): getInviteCodeApi 구현

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/SpaceApi.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/SpaceApi.kt
@@ -1,9 +1,12 @@
 package boostcamp.and07.mindsync.data.network
 
+import boostcamp.and07.mindsync.data.network.request.space.InviteCodeRequest
+import boostcamp.and07.mindsync.data.network.response.space.InviteCodeResponse
 import boostcamp.and07.mindsync.data.network.response.space.SpaceResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
@@ -22,4 +25,9 @@ interface SpaceApi {
     suspend fun getSpace(
         @Path("space_uuid") spaceUuid: String,
     ): Response<SpaceResponse>
+
+    @POST("inviteCodes")
+    suspend fun getInviteCode(
+        @Body inviteCodeRequest: InviteCodeRequest,
+    ): Response<InviteCodeResponse>
 }
