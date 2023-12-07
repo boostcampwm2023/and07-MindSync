d9769ab0c068368c0df4daa3c5aabfcf01661655
jaehan4707 <jahan4707@naver.com>
2023-12-07 12:03:14 +0900

    feat(#155): 보드 목록 요청 API 추가

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/BoardApi.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/BoardApi.kt
@@ -1,6 +1,8 @@
 package boostcamp.and07.mindsync.data.network
 
 import boostcamp.and07.mindsync.data.network.request.board.CreateBoardRequest
+import boostcamp.and07.mindsync.data.network.request.board.GetBoardsRequest
+import boostcamp.and07.mindsync.data.network.response.board.BoardsResponse
 import boostcamp.and07.mindsync.data.network.response.board.CreateBoardResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -10,4 +12,9 @@ interface BoardApi {
     suspend fun createBoard(
         @Body createBoardRequest: CreateBoardRequest,
     ): CreateBoardResponse
+
+    @POST("boards/list")
+    suspend fun getBoards(
+        @Body getBoardsRequest: GetBoardsRequest,
+    ): List<BoardsResponse>
 }
