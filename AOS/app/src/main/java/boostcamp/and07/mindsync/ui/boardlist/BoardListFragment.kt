5d05fcf0aaea20a961c84a8d3e486e519ff7a1e9
jaehan4707 <jahan4707@naver.com>
2023-12-07 12:17:51 +0900

    feat(#154): BoardListFragment 수정
    Hilt 적용
    UiEvent Collcet

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/boardlist/BoardListFragment.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/boardlist/BoardListFragment.kt
@@ -1,17 +1,27 @@
 package boostcamp.and07.mindsync.ui.boardlist
 
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import boostcamp.and07.mindsync.R
 import boostcamp.and07.mindsync.data.model.Board
 import boostcamp.and07.mindsync.databinding.FragmentBoardListBinding
 import boostcamp.and07.mindsync.ui.base.BaseFragment
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class BoardListFragment :
     BaseFragment<FragmentBoardListBinding>(R.layout.fragment_board_list) {
-    private val boardListViewModel = BoardListViewModel()
+    private val boardListViewModel: BoardListViewModel by viewModels()
     private val boardListAdapter = BoardListAdapter()
 
     override fun initView() {
         setBinding()
+        collectBoardEvent()
     }
 
     private fun setBinding() {
@@ -27,5 +37,35 @@ class BoardListFragment :
                 }
             },
         )
+    }
+
+    private fun collectBoardEvent() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                boardListViewModel.event.collectLatest { boardEvent ->
+                    when (boardEvent) {
+                        is BoardUiEvent.Success -> {
+                            Snackbar.make(
+                                binding.root,
+                                "성공",
+                                Snackbar.LENGTH_SHORT,
+                            )
+                                .show()
+                        }
+
+                        is BoardUiEvent.Error -> {
+                            Snackbar.make(
+                                binding.root,
+                                "실패",
+                                Snackbar.LENGTH_SHORT,
+                            )
+                                .show()
+                        }
+
+                        else -> {}
+                    }
+                }
+            }
+        }
     }
 }
