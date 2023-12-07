4daecbb9627348407993711b73823796c815288c
jaehan4707 <jahan4707@naver.com>
2023-12-07 13:56:34 +0900

    refactor(#207):SpaceRepository 반환타입 Flow로 수정

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/space/SpaceRepository.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/space/SpaceRepository.kt
@@ -1,6 +1,7 @@
 package boostcamp.and07.mindsync.data.repository.space
 
 import boostcamp.and07.mindsync.data.model.Space
+import kotlinx.coroutines.flow.Flow
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 
@@ -8,11 +9,11 @@ interface SpaceRepository {
     suspend fun addSpace(
         name: RequestBody,
         icon: MultipartBody.Part,
-    ): Result<Space>
+    ): Flow<Space>
 
-    suspend fun getSpace(spaceUuid: String): Result<Space>
+    suspend fun getSpace(spaceUuid: String): Flow<Space>
 
-    suspend fun getInviteSpaceCode(spaceUuid: String): Result<String>
+    suspend fun getInviteSpaceCode(spaceUuid: String): Flow<String>
 
-    suspend fun joinInviteCode(inviteCode: String): Result<Space>
+    suspend fun joinInviteCode(inviteCode: String): Flow<Space>
 }
