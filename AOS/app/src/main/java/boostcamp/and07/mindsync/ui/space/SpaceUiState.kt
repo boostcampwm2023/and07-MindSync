05f861ace2b17cf10cd0fdee7fb6fe442946e66c
jaehan4707 <jahan4707@naver.com>
2023-12-07 14:11:50 +0900

    feat(#207): SpaceUiState 썸네일파일 추가

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/SpaceUiState.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/SpaceUiState.kt
@@ -1,9 +1,11 @@
 package boostcamp.and07.mindsync.ui.space
 
 import boostcamp.and07.mindsync.data.model.Space
+import java.io.File
 
 data class SpaceUiState(
     val space: Space? = null,
     val spaceName: String = "",
     val spaceThumbnail: String = "",
+    val spaceThumbnailFile: File? = null,
 )
