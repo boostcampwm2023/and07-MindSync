716de949cd2c7b041c2c0d683b3a9ff7e1d091ae
jaehan4707 <jahan4707@naver.com>
2023-12-07 11:50:35 +0900

    feat(#154): createBoardApi 추가

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/BoardApi.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/BoardApi.kt
@@ -1,4 +1,13 @@
 package boostcamp.and07.mindsync.data.network
 
+import boostcamp.and07.mindsync.data.network.request.board.CreateBoardRequest
+import boostcamp.and07.mindsync.data.network.response.board.CreateBoardResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface BoardApi {
+    @POST("boards/create")
+    suspend fun createBoard(
+        @Body createBoardRequest: CreateBoardRequest,
+    ): CreateBoardResponse
 }
