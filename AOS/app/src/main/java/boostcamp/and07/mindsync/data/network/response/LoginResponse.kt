46b31190c193e779c0c86d3f7269f3e21bb2892e
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-12-07 10:26:11 +0900

    feat: login response 양식 바꿈

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/response/LoginResponse.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/response/LoginResponse.kt
@@ -4,9 +4,16 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class LoginResponse(
+data class LoginData(
     @SerialName("access_token")
     val accessToken: String,
     @SerialName("refresh_token")
     val refreshToken: String,
+)
+
+@Serializable
+data class LoginResponse(
+    val statusCode: Int,
+    val message: String,
+    val data: LoginData?,
 )
