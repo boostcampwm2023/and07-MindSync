f63a6a9ab332ad91629f2a849712d163ac275dff
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-12-07 17:55:37 +0900

    refactor(#155): boardAPI 수정

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/BoardApi.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/BoardApi.kt
@@ -1,11 +1,12 @@
 package boostcamp.and07.mindsync.data.network
 
 import boostcamp.and07.mindsync.data.network.request.board.CreateBoardRequest
-import boostcamp.and07.mindsync.data.network.request.board.GetBoardsRequest
 import boostcamp.and07.mindsync.data.network.response.board.BoardsResponse
 import boostcamp.and07.mindsync.data.network.response.board.CreateBoardResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface BoardApi {
     @POST("boards/create")
@@ -13,8 +14,8 @@ interface BoardApi {
         @Body createBoardRequest: CreateBoardRequest,
     ): CreateBoardResponse
 
-    @POST("boards/list")
+    @GET("boards/list")
     suspend fun getBoards(
-        @Body getBoardsRequest: GetBoardsRequest,
+        @Query("spaceId") spaceId: String,
     ): List<BoardsResponse>
 }
