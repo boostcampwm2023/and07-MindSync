04f3c8817760f54aaee42a4257f53bc27001e927
jaehan4707 <jahan4707@naver.com>
2023-12-07 17:28:48 +0900

    feat(#209): getSpaces 구현, Repository, RepositoryImpl

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/profilespace/ProfileSpaceRepository.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/profilespace/ProfileSpaceRepository.kt
@@ -1,8 +1,11 @@
 package boostcamp.and07.mindsync.data.repository.profilespace
 
+import boostcamp.and07.mindsync.data.model.Space
 import boostcamp.and07.mindsync.data.network.response.space.ProfileSpaceJoinData
 import kotlinx.coroutines.flow.Flow
 
 interface ProfileSpaceRepository {
     fun joinSpace(spaceUuid: String): Flow<ProfileSpaceJoinData>
+
+    fun getSpaces(): Flow<List<Space>>
 }
