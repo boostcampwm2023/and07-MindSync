746302ddd9b71df02c845dd13f61cacdc87d8594
jaehan4707 <jahan4707@naver.com>
2023-12-08 04:31:26 +0900

    feat(#222): user 불러오기 구현

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/profilespace/ProfileSpaceRepository.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/profilespace/ProfileSpaceRepository.kt
@@ -2,10 +2,13 @@ package boostcamp.and07.mindsync.data.repository.profilespace
 
 import boostcamp.and07.mindsync.data.model.Space
 import boostcamp.and07.mindsync.data.network.response.space.ProfileSpaceJoinData
+import boostcamp.and07.mindsync.data.network.response.user.UserData
 import kotlinx.coroutines.flow.Flow
 
 interface ProfileSpaceRepository {
     fun joinSpace(spaceUuid: String): Flow<ProfileSpaceJoinData>
 
     fun getSpaces(): Flow<List<Space>>
+
+    fun getSpaceUsers(spaceUuid: String): Flow<List<UserData>>
 }
