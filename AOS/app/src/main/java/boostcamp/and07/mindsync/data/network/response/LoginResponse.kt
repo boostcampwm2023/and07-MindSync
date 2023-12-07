66b29cf8754bac374a5bf0ff94c403ebca4778b0
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-12-07 15:55:43 +0900

    refactor: Response -> Data 순서가 되도록 수정

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/response/LoginResponse.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/response/LoginResponse.kt
@@ -4,17 +4,17 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class LoginData(
-    @SerialName("access_token")
-    val accessToken: String,
-    @SerialName("refresh_token")
-    val refreshToken: String,
-)
-
-@Serializable
 data class LoginResponse(
     val statusCode: Int,
     val message: String,
     val error: String?,
     val data: LoginData?,
+)
+
+@Serializable
+data class LoginData(
+    @SerialName("access_token")
+    val accessToken: String,
+    @SerialName("refresh_token")
+    val refreshToken: String,
 )
--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/response/space/SpaceResponse.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/response/space/SpaceResponse.kt
@@ -3,16 +3,16 @@ package boostcamp.and07.mindsync.data.network.response.space
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class SpaceData(
-    val uuid: String,
-    val name: String? = null,
-    val icon: String? = null,
-)
-
-@Serializable
 data class SpaceResponse(
     val statusCode: Int,
     val message: String,
     val error: String?,
     val data: SpaceData?,
+)
+
+@Serializable
+data class SpaceData(
+    val uuid: String,
+    val name: String? = null,
+    val icon: String? = null,
 )
