472992b1e41ef9a2b2d923d893d0f98b57cfa377
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-12-07 18:21:27 +0900

    feat(#155): 보드 목록 api 요청 작성

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/boardlist/BoardListRepositoryImpl.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/repository/boardlist/BoardListRepositoryImpl.kt
@@ -8,48 +8,48 @@ import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class BoardListRepositoryImpl
-    @Inject
-    constructor(
-        private val boardApi: BoardApi,
-    ) : BoardListRepository {
-        override fun createBoard(
-            boardName: String,
-            spaceId: String,
-            imageUrl: String,
-        ): Flow<Board> =
-            flow {
-                val response =
-                    boardApi.createBoard(
-                        CreateBoardRequest(
-                            boardName,
-                            spaceId,
-                            imageUrl,
-                        ),
-                    )
-                response.data?.let { data ->
-                    emit(
-                        Board(
-                            id = data.boardId,
-                            name = boardName,
-                            date = data.date,
-                            imageUrl = imageUrl,
-                        ),
-                    )
-                }
-            }
-
-        override fun getBoard(spaceId: String): Flow<List<Board>> =
-            flow {
-                val response = boardApi.getBoards(GetBoardsRequest(spaceId))
+@Inject
+constructor(
+    private val boardApi: BoardApi,
+) : BoardListRepository {
+    override fun createBoard(
+        boardName: String,
+        spaceId: String,
+        imageUrl: String,
+    ): Flow<Board> =
+        flow {
+            val response =
+                boardApi.createBoard(
+                    CreateBoardRequest(
+                        boardName,
+                        spaceId,
+                        imageUrl,
+                    ),
+                )
+            response.data?.let { data ->
                 emit(
-                    response.map { boardsResponse ->
-                        Board(
-                            id = boardsResponse.boardId,
-                            name = boardsResponse.boardName,
-                            date = boardsResponse.createdAt,
-                            imageUrl = boardsResponse.imageUrl,
-                        )
-                    },
+                    Board(
+                        id = data.boardId,
+                        name = boardName,
+                        date = data.date,
+                        imageUrl = imageUrl,
+                    ),
                 )
             }
-    }
+        }
+
+    override fun getBoard(spaceId: String): Flow<List<Board>> =
+        flow {
+            val response = boardApi.getBoards(spaceId)
+            emit(
+                response.data.map { board ->
+                    Board(
+                        id = board.boardId,
+                        name = board.boardName,
+                        date = board.createdAt,
+                        imageUrl = board.imageUrl,
+                    )
+                },
+            )
+        }
+}
