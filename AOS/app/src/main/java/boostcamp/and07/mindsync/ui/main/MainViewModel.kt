6d8ae860beb790b13e303be59c989fda7d6df644
jaehan4707 <jahan4707@naver.com>
2023-12-07 17:29:14 +0900

    feat(#209): mainViewModel getSpaces 구현

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/main/MainViewModel.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/main/MainViewModel.kt
@@ -1,8 +1,10 @@
 package boostcamp.and07.mindsync.ui.main
 
 import androidx.lifecycle.viewModelScope
+import boostcamp.and07.mindsync.data.model.Space
 import boostcamp.and07.mindsync.data.repository.login.LogoutEventRepository
 import boostcamp.and07.mindsync.data.repository.profile.ProfileRepository
+import boostcamp.and07.mindsync.data.repository.profilespace.ProfileSpaceRepository
 import boostcamp.and07.mindsync.ui.base.BaseActivityViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineExceptionHandler
@@ -19,12 +21,15 @@ class MainViewModel
     @Inject
     constructor(
         private val profileRepository: ProfileRepository,
+        private val profileSpaceRepository: ProfileSpaceRepository,
         private val logoutEventRepository: LogoutEventRepository,
     ) : BaseActivityViewModel(logoutEventRepository) {
         private val _profileImageUrl = MutableStateFlow("")
         val profileImageUrl: StateFlow<String> = _profileImageUrl
         private val _event = MutableSharedFlow<MainUiEvent>()
         val event: SharedFlow<MainUiEvent> = _event
+        private val _spaces = MutableStateFlow<List<Space>>(listOf())
+        val spaces: StateFlow<List<Space>> = _spaces
         private val coroutineExceptionHandler =
             CoroutineExceptionHandler { _, throwable ->
                 viewModelScope.launch { _event.emit(MainUiEvent.ShowMessage(throwable.message.toString())) }
@@ -34,6 +39,14 @@ class MainViewModel
             viewModelScope.launch(coroutineExceptionHandler) {
                 profileRepository.getProfile().collectLatest { profile ->
                     _profileImageUrl.value = profile.imageUrl
+                }
+            }
+        }
+
+        fun getSpaces() {
+            viewModelScope.launch(coroutineExceptionHandler) {
+                profileSpaceRepository.getSpaces().collectLatest { spaces ->
+                    _spaces.value = spaces
                 }
             }
         }
