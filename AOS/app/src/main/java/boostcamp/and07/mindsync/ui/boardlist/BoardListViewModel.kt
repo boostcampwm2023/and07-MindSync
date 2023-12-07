131ffe81da2e6274388c50c304efe7927b384a36
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-12-08 02:23:28 +0900

    refactor: sharedFlow의 update 함수를 활용하도록 변경
    Co-authored-by: jaehan4707 <jahan4707@naver.com>

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/boardlist/BoardListViewModel.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/boardlist/BoardListViewModel.kt
@@ -18,74 +18,78 @@ import javax.inject.Inject
 
 @HiltViewModel
 class BoardListViewModel
-@Inject
-constructor(
-    private val boardListRepository: BoardListRepository,
-) : ViewModel() {
-    private val _boardUiState = MutableStateFlow(BoardUiState())
-    val boardUiState: StateFlow<BoardUiState> = _boardUiState
-    private val _boardUiEvent = MutableSharedFlow<BoardUiEvent>()
-    val boardUiEvent: SharedFlow<BoardUiEvent> = _boardUiEvent
+    @Inject
+    constructor(
+        private val boardListRepository: BoardListRepository,
+    ) : ViewModel() {
+        private val _boardUiState = MutableStateFlow(BoardUiState())
+        val boardUiState: StateFlow<BoardUiState> = _boardUiState
+        private val _boardUiEvent = MutableSharedFlow<BoardUiEvent>()
+        val boardUiEvent: SharedFlow<BoardUiEvent> = _boardUiEvent
 
-    private val coroutineExceptionHandler =
-        CoroutineExceptionHandler { _, throwable ->
-            viewModelScope.launch { _boardUiEvent.emit(BoardUiEvent.Error(throwable.message.toString())) }
-        }
-
-    init {
-        getBoards()
-    }
-
-    fun addBoard(
-        part: MultipartBody.Part,
-        name: String,
-    ) {
-        viewModelScope.launch(coroutineExceptionHandler) {
-            boardListRepository.createBoard(
-                boardName = name,
-                spaceId = TEST_SPACE_ID,
-                imageUrl = TEST_IMAGE_URL,
-            ).collectLatest { board ->
-                val newBoards = _boardUiState.value.boards.toMutableList().apply { add(board) }
-                _boardUiState.value = _boardUiState.value.copy(boards = newBoards)
-                _boardUiEvent.emit(BoardUiEvent.Success)
+        private val coroutineExceptionHandler =
+            CoroutineExceptionHandler { _, throwable ->
+                viewModelScope.launch { _boardUiEvent.emit(BoardUiEvent.Error(throwable.message.toString())) }
             }
-        }
-    }
 
-    private fun getBoards() {
-        viewModelScope.launch(coroutineExceptionHandler) {
-            boardListRepository.getBoard(TEST_SPACE_ID).collectLatest { list ->
-                _boardUiState.update { it ->
-                    it.copy(boards = list)
+        init {
+            getBoards()
+        }
+
+        fun addBoard(
+            part: MultipartBody.Part,
+            name: String,
+        ) {
+            viewModelScope.launch(coroutineExceptionHandler) {
+                boardListRepository.createBoard(
+                    boardName = name,
+                    spaceId = TEST_SPACE_ID,
+                    imageUrl = TEST_IMAGE_URL,
+                ).collectLatest { board ->
+                    _boardUiState.update { boardUiState ->
+                        val newBoards = boardUiState.boards.toMutableList().apply { add(board) }
+                        boardUiState.copy(boards = newBoards)
+                    }
+                    _boardUiEvent.emit(BoardUiEvent.Success)
                 }
-                _boardUiEvent.emit(BoardUiEvent.Success)
             }
         }
-    }
 
-    fun selectBoard(selectBoard: Board) {
-        val newSelectBoards =
-            _boardUiState.value.boards.toMutableList().filter { board -> board.isChecked }
-        _boardUiState.value =
-            _boardUiState.value.copy(
-                selectBoards = newSelectBoards,
-            )
-    }
+        private fun getBoards() {
+            viewModelScope.launch(coroutineExceptionHandler) {
+                boardListRepository.getBoard(TEST_SPACE_ID).collectLatest { list ->
+                    _boardUiState.update { it ->
+                        it.copy(boards = list)
+                    }
+                    _boardUiEvent.emit(BoardUiEvent.Success)
+                }
+            }
+        }
 
-    fun deleteBoard() {
-        val newBoards =
-            _boardUiState.value.boards.toMutableList().filter { board -> !board.isChecked }
-        _boardUiState.value =
-            _boardUiState.value.copy(
-                boards = newBoards,
-                selectBoards = listOf(),
-            )
-    }
+        fun selectBoard(selectBoard: Board) {
+            _boardUiState.update { boardUiState ->
+                val newSelectBoards =
+                    boardUiState.boards.toMutableList().filter { board -> board.isChecked }
+                boardUiState.copy(
+                    selectBoards = newSelectBoards,
+                )
+            }
+        }
 
-    companion object {
-        private const val TEST_SPACE_ID = "11ee94cb588902308d61176844e12449"
-        private const val TEST_IMAGE_URL =
-            "https://image.yes24.com/blogimage/blog/w/o/woojukaki/IMG_20201015_182419.jpg"
+        fun deleteBoard() {
+            _boardUiState.update { boardUiState ->
+                val newBoards =
+                    boardUiState.boards.toMutableList().filter { board -> !board.isChecked }
+                boardUiState.copy(
+                    boards = newBoards,
+                    selectBoards = listOf(),
+                )
+            }
+        }
+
+        companion object {
+            private const val TEST_SPACE_ID = "11ee94cb588902308d61176844e12449"
+            private const val TEST_IMAGE_URL =
+                "https://image.yes24.com/blogimage/blog/w/o/woojukaki/IMG_20201015_182419.jpg"
+        }
     }
-}
