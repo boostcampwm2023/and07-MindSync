86cb361475585949bcacadcbaa4e6a38be57e3a4
jaehan4707 <jahan4707@naver.com>
2023-12-07 23:33:47 +0900

    style(#209): ktlint 적용

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/list/SpaceListAdapter.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/list/SpaceListAdapter.kt
@@ -1,6 +1,5 @@
 package boostcamp.and07.mindsync.ui.boardlist
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
