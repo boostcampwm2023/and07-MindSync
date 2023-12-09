d518ce30257c206fad7641f1a7403a64d395d431
jaehan4707 <jahan4707@naver.com>
2023-12-09 20:12:50 +0900

    fix(#229): ProfileUiEvent 추가

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/profile/ProfileUiEvent.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/profile/ProfileUiEvent.kt
@@ -4,4 +4,6 @@ sealed class ProfileUiEvent {
     data object NavigateToBack : ProfileUiEvent()
 
     data class ShowMessage(val message: String) : ProfileUiEvent()
+
+    data object UpdateProfileNickName: ProfileUiEvent()
 }
