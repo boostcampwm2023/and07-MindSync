47d7b924fa14df2cf06ded7ec02416a7b0fcbe02
jaehan4707 <jahan4707@naver.com>
2023-12-09 20:49:17 +0900

    fix(#229): createBoardApi 수정 MultiPart 적용

--- AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/BoardApi.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/data/network/BoardApi.kt
@@ -1,20 +1,26 @@
 package boostcamp.and07.mindsync.data.network
 
-import boostcamp.and07.mindsync.data.network.request.board.CreateBoardRequest
 import boostcamp.and07.mindsync.data.network.request.board.DeleteBoardRequest
 import boostcamp.and07.mindsync.data.network.response.board.BoardsResponse
 import boostcamp.and07.mindsync.data.network.response.board.CreateBoardResponse
 import boostcamp.and07.mindsync.data.network.response.board.DeleteBoardResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Part
 import retrofit2.http.Query
 
 interface BoardApi {
     @POST("boards/create")
+    @Multipart
     suspend fun createBoard(
-        @Body createBoardRequest: CreateBoardRequest,
+        @Part("boardName") boardName: RequestBody,
+        @Part("spaceId") spaceId: RequestBody,
+        @Part image: MultipartBody.Part?,
     ): CreateBoardResponse
 
     @GET("boards/list")
