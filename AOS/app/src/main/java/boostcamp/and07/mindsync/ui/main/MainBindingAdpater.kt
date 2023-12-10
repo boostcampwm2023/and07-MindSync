a1345b83ced1217568caa93d07221ba60961e819
jaehan4707 <jahan4707@naver.com>
2023-12-11 00:39:58 +0900

    refactor(#229): sideBard Space DataBinding으로 변경

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/main/MainBindingAdpater.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/main/MainBindingAdpater.kt
@@ -2,6 +2,7 @@ package boostcamp.and07.mindsync.ui.main
 
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import boostcamp.and07.mindsync.data.model.Space
 import boostcamp.and07.mindsync.data.network.response.user.UserData
 import boostcamp.and07.mindsync.ui.boardlist.UsersAdapter
 
@@ -9,5 +10,11 @@ import boostcamp.and07.mindsync.ui.boardlist.UsersAdapter
 fun RecyclerView.bindUsers(users: List<UserData>) {
     if (this.adapter != null) {
         (this.adapter as UsersAdapter).submitList(users.toMutableList())
+    }
+}
+@BindingAdapter("app:sideBarSpaces")
+fun RecyclerView.bindSpaces(spaces:List<Space>){
+    if(this.adapter !=null){
+        (this.adapter as SideBarSpaceAdapter).submitList(spaces.toMutableList())
     }
 }
