61fb2265396cafd9aa7f977db9bab9a670c08c88
dltkd1395 <dltkd13956@naver.com>
2023-12-11 18:12:03 +0900

    style(#245): ktlint 적용

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/profile/ProfileViewModel.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/profile/ProfileViewModel.kt
@@ -21,79 +21,79 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel
-@Inject
-constructor(
-    private val profileRepository: ProfileRepository,
-    logoutEventRepository: LogoutEventRepository,
-) :
+    @Inject
+    constructor(
+        private val profileRepository: ProfileRepository,
+        logoutEventRepository: LogoutEventRepository,
+    ) :
     BaseActivityViewModel(logoutEventRepository) {
-    private val _uiState = MutableStateFlow(ProfileUiState())
-    val uiState: StateFlow<ProfileUiState> = _uiState
-    private val _event = MutableSharedFlow<ProfileUiEvent>()
-    val event: SharedFlow<ProfileUiEvent> = _event
-    private val coroutineExceptionHandler =
-        CoroutineExceptionHandler { _, throwable ->
-            viewModelScope.launch { _event.emit(ProfileUiEvent.ShowMessage(throwable.message.toString())) }
-        }
-
-    init {
-        fetchProfile()
-    }
-
-    fun updateProfileUri(uri: Uri) {
-        _uiState.update { uiState ->
-            uiState.copy(imageUri = uri)
-        }
-    }
-
-    fun updateNickName(nickname: CharSequence) {
-        _uiState.update { uiState ->
-            uiState.copy(nickname = nickname.toString(), isModify = isProfileEditDisabled(nickname))
-        }
-        viewModelScope.launch {
-            _event.emit(ProfileUiEvent.UpdateProfileNickName)
-        }
-    }
-
-    private fun isProfileEditDisabled(nickname: CharSequence) =
-        uiState.value.serverFetchedNickName != nickname.toString() && nickname.isNotEmpty() || uiState.value.imageFile != null
-
-    fun setProfileImageFile(file: File) {
-        _uiState.update { uiState ->
-            val isNicknameNotEmpty = uiState.nickname.isNotEmpty()
-            uiState.copy(
-                imageFile = file,
-                isModify = isNicknameNotEmpty || uiState.isModify,
-            )
-        }
-    }
-
-    fun updateProfile(imageName: String) {
-        val image =
-            _uiState.value.imageFile?.let { file ->
-                fileToMultiPart(file, imageName)
+        private val _uiState = MutableStateFlow(ProfileUiState())
+        val uiState: StateFlow<ProfileUiState> = _uiState
+        private val _event = MutableSharedFlow<ProfileUiEvent>()
+        val event: SharedFlow<ProfileUiEvent> = _event
+        private val coroutineExceptionHandler =
+            CoroutineExceptionHandler { _, throwable ->
+                viewModelScope.launch { _event.emit(ProfileUiEvent.ShowMessage(throwable.message.toString())) }
             }
-        val nickname = _uiState.value.nickname.toRequestBody()
 
-        viewModelScope.launch(coroutineExceptionHandler) {
-            profileRepository.patchProfile(nickname, image).collectLatest {
-                _event.emit(ProfileUiEvent.NavigateToBack)
+        init {
+            fetchProfile()
+        }
+
+        fun updateProfileUri(uri: Uri) {
+            _uiState.update { uiState ->
+                uiState.copy(imageUri = uri)
             }
         }
-    }
 
-    private fun fetchProfile() {
-        viewModelScope.launch(coroutineExceptionHandler) {
-            profileRepository.getProfile().collectLatest { profile ->
-                _uiState.update { uiState ->
-                    uiState.copy(
-                        serverFetchedImage = Uri.parse(profile.imageUrl),
-                        serverFetchedNickName = profile.nickname,
-                        nickname = profile.nickname,
-                        imageUri = Uri.parse(profile.imageUrl),
-                    )
+        fun updateNickName(nickname: CharSequence) {
+            _uiState.update { uiState ->
+                uiState.copy(nickname = nickname.toString(), isModify = isProfileEditDisabled(nickname))
+            }
+            viewModelScope.launch {
+                _event.emit(ProfileUiEvent.UpdateProfileNickName)
+            }
+        }
+
+        private fun isProfileEditDisabled(nickname: CharSequence) =
+            uiState.value.serverFetchedNickName != nickname.toString() && nickname.isNotEmpty() || uiState.value.imageFile != null
+
+        fun setProfileImageFile(file: File) {
+            _uiState.update { uiState ->
+                val isNicknameNotEmpty = uiState.nickname.isNotEmpty()
+                uiState.copy(
+                    imageFile = file,
+                    isModify = isNicknameNotEmpty || uiState.isModify,
+                )
+            }
+        }
+
+        fun updateProfile(imageName: String) {
+            val image =
+                _uiState.value.imageFile?.let { file ->
+                    fileToMultiPart(file, imageName)
+                }
+            val nickname = _uiState.value.nickname.toRequestBody()
+
+            viewModelScope.launch(coroutineExceptionHandler) {
+                profileRepository.patchProfile(nickname, image).collectLatest {
+                    _event.emit(ProfileUiEvent.NavigateToBack)
+                }
+            }
+        }
+
+        private fun fetchProfile() {
+            viewModelScope.launch(coroutineExceptionHandler) {
+                profileRepository.getProfile().collectLatest { profile ->
+                    _uiState.update { uiState ->
+                        uiState.copy(
+                            serverFetchedImage = Uri.parse(profile.imageUrl),
+                            serverFetchedNickName = profile.nickname,
+                            nickname = profile.nickname,
+                            imageUri = Uri.parse(profile.imageUrl),
+                        )
+                    }
                 }
             }
         }
     }
-}
