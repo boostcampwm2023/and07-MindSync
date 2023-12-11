61ee0632ada6680fc72abcf1ac1cbdfacdda3b63
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-12-11 20:40:22 +0900

    feat(#249): View.setClickEvent에 throttleFirst 추가

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/util/FlowExtension.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/util/FlowExtension.kt
@@ -1,9 +1,26 @@
 package boostcamp.and07.mindsync.ui.util
 
+import android.view.View
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 const val THROTTLE_DURATION = 200L
+
+fun View.setClickEvent(
+    uiScope: CoroutineScope,
+    windowDuration: Long = THROTTLE_DURATION,
+    onClick: () -> Unit,
+) {
+    clicks()
+        .throttleFirst(windowDuration)
+        .onEach { onClick.invoke() }
+        .launchIn(uiScope)
+}
 
 fun <T> Flow<T>.throttleFirst(windowDuration: Long = THROTTLE_DURATION): Flow<T> =
     flow {
@@ -15,4 +32,12 @@ fun <T> Flow<T>.throttleFirst(windowDuration: Long = THROTTLE_DURATION): Flow<T>
                 emit(upstream)
             }
         }
+    }
+
+fun View.clicks(): Flow<Unit> =
+    callbackFlow {
+        setOnClickListener {
+            this.trySend(Unit)
+        }
+        awaitClose { setOnClickListener(null) }
     }
