63843f5c6b0e57864d40b65c26ef5f845d9fab05
jaehan4707 <jahan4707@naver.com>
2023-12-13 00:59:57 +0900

    feat(#280): RecycleBinFragment 구현

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/recyclebin/RecycleBinFragment.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/recyclebin/RecycleBinFragment.kt
@@ -1,10 +1,52 @@
 package boostcamp.and07.mindsync.ui.recyclebin
 
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import boostcamp.and07.mindsync.R
+import boostcamp.and07.mindsync.data.model.Board
 import boostcamp.and07.mindsync.databinding.FragmentRecycleBinBinding
 import boostcamp.and07.mindsync.ui.base.BaseFragment
+import boostcamp.and07.mindsync.ui.boardlist.BoardClickListener
+import boostcamp.and07.mindsync.ui.boardlist.BoardListAdapter
+import boostcamp.and07.mindsync.ui.boardlist.BoardListFragmentDirections
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RecycleBinFragment : BaseFragment<FragmentRecycleBinBinding>(R.layout.fragment_recycle_bin) {
+    private val recycleBinViewModel: RecycleBinViewModel by viewModels()
+    private val boardListAdapter = BoardListAdapter()
+    private val args: RecycleBinFragmentArgs by navArgs()
+
     override fun initView() {
+        setBinding()
+        setBoardRestoreButton()
+        recycleBinViewModel.setSpace(args.spaceId)
+    }
+
+    private fun setBinding() {
+        binding.vm = recycleBinViewModel
+        binding.rvRecyclebinBoard.adapter = boardListAdapter
+        boardListAdapter.setBoardClickListener(
+            object : BoardClickListener {
+                override fun onClick(board: Board) {
+                    findNavController().navigate(
+                        BoardListFragmentDirections.actionBoardListFragmentToMindMapFragment(
+                            board.id,
+                        ),
+                    )
+                }
+
+                override fun onCheckBoxClick(board: Board) {
+                    recycleBinViewModel.selectBoard(board)
+                }
+            },
+        )
+    }
+
+    private fun setBoardRestoreButton() {
+        binding.btnRecyclebinRestore.setOnClickListener {
+            recycleBinViewModel.restoreBoard()
+        }
     }
 }
