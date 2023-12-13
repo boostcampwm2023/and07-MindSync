c2762110e5ae3e2562549e4a4ded44eb9e345acf
tnpfldyd <dkdldhs0322@gmail.com>
2023-12-13 18:14:14 +0900

    fix(#212): 최대 길이 추가

--- nestjs-BE/server/src/spaces/dto/create-space.dto.ts
+++ nestjs-BE/server/src/spaces/dto/create-space.dto.ts
@@ -1,9 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { MAX_NAME_LENGTH } from 'src/config/magic-number';
 
 export class CreateSpaceDto {
   @IsString()
   @IsNotEmpty()
+  @MaxLength(MAX_NAME_LENGTH)
   @ApiProperty({ example: 'Sample Space', description: 'Name of the space' })
   name: string;
 
