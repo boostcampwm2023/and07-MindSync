6643c94b4c249349932e6fd49a93470ec11728d6
Conut-1 <1mim1@naver.com>
2023-12-13 17:44:41 +0900

    feat: 보드 이름 최대 길이 추가

--- nestjs-BE/server/src/boards/dto/create-board.dto.ts
+++ nestjs-BE/server/src/boards/dto/create-board.dto.ts
@@ -1,10 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { MAX_NAME_LENGTH } from 'src/config/magic-number';
 
 export class CreateBoardDto {
   @ApiProperty({ description: '보드 이름' })
   @IsString()
   @IsNotEmpty()
+  @MaxLength(MAX_NAME_LENGTH)
   boardName: string;
 
   @ApiProperty({ description: '스페이스 id' })
--- nestjs-BE/server/src/config/magic-number.ts
+++ nestjs-BE/server/src/config/magic-number.ts
@@ -10,3 +10,4 @@ export const REFRESH_TOKEN_EXPIRY_DAYS = 14;
 export const INVITE_CODE_CACHE_SIZE = 5000;
 export const INVITE_CODE_LENGTH = 10;
 export const INVITE_CODE_EXPIRY_HOURS = 6;
+export const MAX_NAME_LENGTH = 20;
