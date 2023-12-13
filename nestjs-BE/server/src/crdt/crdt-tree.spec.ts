651021540a84336cbdfca4ec0b13cbd5a6824fa7
Conut-1 <1mim1@naver.com>
2023-12-13 21:06:14 +0900

    feat: CrdtTree 테스트 추가

---
+++ nestjs-BE/server/src/crdt/crdt-tree.spec.ts
@@ -0,0 +1,61 @@
+import { CrdtTree } from './crdt-tree';
+
+it('crdt tree 동기화', () => {
+  const tree1 = new CrdtTree<string>('1');
+  const tree2 = new CrdtTree<string>('2');
+
+  const op_1_1 = tree1.generateOperationAdd('a', 'root', 'hello');
+  const op_1_2 = tree1.generateOperationAdd('b', 'root', 'hi');
+
+  const op_2_1 = tree2.generateOperationAdd('c', 'root', 'good');
+  const op_2_2 = tree2.generateOperationAdd('d', 'c', 'bad');
+
+  tree1.applyOperations([op_1_1, op_1_2]);
+  tree2.applyOperations([op_2_1, op_2_2]);
+
+  tree2.applyOperations([op_1_1, op_1_2]);
+  tree1.applyOperations([op_2_1, op_2_2]);
+
+  const op_1_3 = tree1.generateOperationUpdate('a', 'updatedByTree1');
+  const op_1_4 = tree1.generateOperationMove('d', 'b');
+
+  const op_2_3 = tree2.generateOperationUpdate('a', 'updatedByTree2');
+  const op_2_4 = tree2.generateOperationMove('a', 'c');
+
+  tree1.applyOperations([op_1_3, op_1_4]);
+  tree2.applyOperations([op_2_3, op_2_4]);
+
+  tree2.applyOperations([op_1_3, op_1_4]);
+  tree1.applyOperations([op_2_3, op_2_4]);
+
+  const op_1_5 = tree1.generateOperationDelete('b');
+
+  const op_2_5 = tree2.generateOperationUpdate('b', 'updatedByTree2');
+
+  tree1.applyOperations([op_1_5]);
+  tree2.applyOperations([op_2_5]);
+
+  tree2.applyOperations([op_1_5]);
+  tree1.applyOperations([op_2_5]);
+
+  tree1.clock.id = tree2.clock.id;
+
+  expect(tree1).toMatchObject(tree2);
+});
+
+it('crdt tree 역직렬화', () => {
+  const tree = new CrdtTree<string>('1');
+
+  const op1 = tree.generateOperationAdd('a', 'root', 'hello');
+  const op2 = tree.generateOperationAdd('b', 'root', 'hi');
+  const op3 = tree.generateOperationAdd('c', 'root', 'good');
+  const op4 = tree.generateOperationAdd('d', 'c', 'bad');
+
+  tree.applyOperations([op1, op2, op3, op4]);
+
+  expect(JSON.stringify(tree));
+
+  const parsedTree = CrdtTree.parse<string>(JSON.stringify(tree));
+
+  expect(JSON.stringify(tree)).toEqual(JSON.stringify(parsedTree));
+});
