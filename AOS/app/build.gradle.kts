d4be19909f8f415e4ecf20321566aa55c20ec3e5
dltkd1395 <dltkd13956@naver.com>
2024-01-12 18:04:13 +0900

    chore(#312): compose 관련 라이브러리 추가

--- AOS/app/build.gradle.kts
+++ AOS/app/build.gradle.kts
@@ -103,6 +103,22 @@ dependencies {
     implementation("com.google.android.flexbox:flexbox:3.0.0")
     // lottie
     implementation("com.airbnb.android:lottie:6.2.0")
+
+    // compose
+    val composePlatform = platform("androidx.compose:compose-bom:2023.10.01")
+    implementation(composePlatform)
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("com.google.accompanist:accompanist-permissions:0.32.0")
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
 
 val ktlintCheck by tasks.registering(JavaExec::class) {
