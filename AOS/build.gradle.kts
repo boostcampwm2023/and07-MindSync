7c3eddec13c3188f54f6c742ba4b8317616a1bff
dltkd1395 <dltkd13956@naver.com>
2024-01-12 18:03:05 +0900

    chore(#312): kotlin version 1.9.0 -> 1.9.21로 변경

--- AOS/build.gradle.kts
+++ AOS/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1" apply false
     kotlin("plugin.serialization") version "1.9.0"
