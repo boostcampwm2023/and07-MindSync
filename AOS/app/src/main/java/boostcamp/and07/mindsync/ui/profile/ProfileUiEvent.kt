ad807a97cdd2e845e11a701d4524badb2bb6c4d1
dltkd1395 <dltkd13956@naver.com>
2024-01-13 04:02:55 +0900

    feat(#313): ProfileUiEvent None 추가

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/profile/ProfileUiEvent.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/profile/ProfileUiEvent.kt
@@ -6,4 +6,6 @@ sealed class ProfileUiEvent {
     data class ShowMessage(val message: String) : ProfileUiEvent()
 
     data object UpdateProfileNickName : ProfileUiEvent()
+
+    data object None : ProfileUiEvent()
 }
