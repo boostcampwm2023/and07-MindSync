4dab99a5015ffcef621fa32f43a5a6a31837e967
dltkd1395 <dltkd13956@naver.com>
2024-01-12 18:08:24 +0900

    feat(#312): BaseComposeActivity 구현

---
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/base/BaseComposeActivity.kt
@@ -0,0 +1,19 @@
+package boostcamp.and07.mindsync.ui.base
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+
+abstract class BaseComposeActivity : ComponentActivity() {
+
+    @Composable
+    abstract fun Content()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Content()
+        }
+    }
+}
