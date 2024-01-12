149577cb032d4a52661eac3c0ae7e0b1bd16cdcb
dltkd1395 <dltkd13956@naver.com>
2024-01-12 18:07:04 +0900

    feat(#312): Color 추가

---
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/theme/Color.kt
@@ -0,0 +1,29 @@
+package boostcamp.and07.mindsync.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF222227)
+val Gray6 = Color(0xFF333232)
+val Gray5 = Color(0xFF6A6A6A)
+val Gray4 = Color(0xFF949494)
+val Gray3 = Color(0xFFD9D9D9)
+val Gray2 = Color(0xFFF4F4F4)
+val Gray1 = Color(0xFFF9F9F9)
+
+val Red3 = Color(0xFFFB5D67)
+val Red2 = Color(0xFFFF8080)
+val Red1 = Color(0xFFE1719F)
+
+val Blue1 = Color(0xFF007EC2)
+
+val Green6 = Color(0xFF336770)
+val Green5 = Color(0xFF4B877B)
+val Green4 = Color(0xFF7AA37D)
+val Green3 = Color(0xFF49AA95)
+val Green2 = Color(0xFFB9BC81)
+val Green1 = Color(0xFFC7FACF)
+
+val Yellow4 = Color(0xFFFFCF96)
+val Yellow3 = Color(0xFFFFECCC)
+val Yellow2 = Color(0xFFF2FDBB)
+val Yellow1 = Color(0xFFEEE9E9)
