962424ab561f9474efba40bfe357ed0c733f3e88
jaehan4707 <jahan4707@naver.com>
2024-01-20 19:22:21 +0900

    refactor(#317): BoardUiEvent 개선

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/boardlist/BoardUiEvent.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/boardlist/BoardUiEvent.kt
@@ -1,7 +1,7 @@
 package boostcamp.and07.mindsync.ui.boardlist
 
 sealed class BoardUiEvent {
-    data object Success : BoardUiEvent()
+    data object NavigateToBack : BoardUiEvent()
 
-    data class Error(val message: String) : BoardUiEvent()
+    data class ShowMessage(val message: String) : BoardUiEvent()
 }
