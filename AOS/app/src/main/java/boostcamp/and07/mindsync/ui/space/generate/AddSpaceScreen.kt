cfae3e91f35544eaf373cd0db7d972f09789a1bd
jaehan4707 <jahan4707@naver.com>
2024-02-19 22:15:34 +0900

    refactor(#322): imageLauncher 분리, Preview 활성화

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/generate/AddSpaceScreen.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/generate/AddSpaceScreen.kt
@@ -37,11 +37,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import boostcamp.and07.mindsync.R
 import boostcamp.and07.mindsync.ui.space.SpaceUiState
 import boostcamp.and07.mindsync.ui.theme.Blue1
+import boostcamp.and07.mindsync.ui.theme.MindSyncTheme
 import coil.compose.AsyncImage
 
 @Composable
@@ -53,6 +55,24 @@ fun AddSpaceScreen(
     createImage: (Uri) -> Unit,
 ) {
     val uiState by addSpaceViewModel.uiState.collectAsStateWithLifecycle()
+
+    AddSpaceContent(
+        onBackClicked = onBackClicked,
+        uiState = uiState,
+        createSpace = createSpace,
+        updateSpaceName = updateSpaceName,
+        createImage = createImage,
+    )
+}
+
+@Composable
+fun AddSpaceContent(
+    onBackClicked: () -> Unit = { },
+    uiState: SpaceUiState = SpaceUiState(),
+    createSpace: (String) -> Unit = {},
+    updateSpaceName: (String) -> Unit = {},
+    createImage: (Uri) -> Unit = {},
+) {
     val imageLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.PickVisualMedia(),
         onResult = { uri ->
@@ -61,23 +81,6 @@ fun AddSpaceScreen(
             }
         },
     )
-    AddSpaceContent(
-        onBackClicked = onBackClicked,
-        uiState = uiState,
-        imageLauncher = imageLauncher,
-        createSpace = createSpace,
-        updateSpaceName = updateSpaceName,
-    )
-}
-
-@Composable
-fun AddSpaceContent(
-    onBackClicked: () -> Unit = { },
-    uiState: SpaceUiState = SpaceUiState(),
-    imageLauncher: ManagedActivityResultLauncher<PickVisualMediaRequest, Uri?>,
-    createSpace: (String) -> Unit = {},
-    updateSpaceName: (String) -> Unit = {},
-) {
     AddSpaceTopBar(onBackClicked)
     Row(
         modifier = Modifier
@@ -210,5 +213,13 @@ fun SpaceNameInputButton(createSpace: (String) -> Unit) {
         createSpace(icon)
     }, Modifier.width(264.dp)) {
         Text(text = stringResource(id = R.string.check_message))
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun AddSpacePreview() {
+    MindSyncTheme {
+        AddSpaceContent()
     }
 }
