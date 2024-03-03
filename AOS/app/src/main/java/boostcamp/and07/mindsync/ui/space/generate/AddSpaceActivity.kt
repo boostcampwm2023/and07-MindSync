3f31545475e1189b2de359a9fbbc7e2ae57bc299
jaehan4707 <jahan4707@naver.com>
2024-03-03 15:31:47 +0900

    chore(#322): 기존 AddSpaceActivity 코드 제

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/generate/AddSpaceActivity.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/space/generate/AddSpaceActivity.kt
@@ -13,47 +13,6 @@ import java.io.File
 class AddSpaceActivity : BaseComposeActivity() {
     private val addSpaceViewModel: AddSpaceViewModel by viewModels()
 
-//    override fun init() {
-//        //setBinding()
-//        setBackBtn()
-//        collectSpaceEvent()
-//        //setClickEventThrottle()
-//    }
-
-//    override fun getViewModel(): BaseActivityViewModel {
-//        return addSpaceViewModel
-//    }
-//
-//    private fun setBinding() {
-//        binding.vm = addSpaceViewModel
-//    }
-//
-//    private fun setClickEventThrottle() {
-//        binding.imgbtnUpdateSpaceThumbnail.setClickEvent(lifecycleScope) {
-//            imagePickerHandler.checkPermissionsAndLaunchImagePicker()
-//        }
-//    }
-
-//    private fun collectSpaceEvent() {
-//        lifecycleScope.launch {
-//            repeatOnLifecycle(Lifecycle.State.STARTED) {
-//                addSpaceViewModel.event.collectLatest { spaceEvent ->
-//                    when (spaceEvent) {
-//                        is SpaceUiEvent.SuccessAdd -> {
-//                            finish()
-//                        }
-//
-//                        is SpaceUiEvent.ShowMessage -> { // 에러일 경우가 잇나?
-//                            showMessage(spaceEvent.message)
-//                        }
-//
-//                        else -> {}
-//                    }
-//                }
-//            }
-//        }
-//    }
-
     private fun createImage(uri: Uri?) {
         uri?.let { uri ->
             val file = File(uri.toAbsolutePath(this))
@@ -74,17 +33,4 @@ class AddSpaceActivity : BaseComposeActivity() {
             )
         }
     }
-
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<out String>,
-//        grantResults: IntArray,
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if (requestCode == ImagePickerHandler.REQUEST_CODE_PERMISSIONS &&
-//            grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
-//        ) {
-//            imagePickerHandler.launchImagePicker()
-//        }
-//    }
 }
