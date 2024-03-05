08bc8c09de5386050c8b020326b788cf575fab08
jaehan4707 <jahan4707@naver.com>
2024-03-05 21:51:44 +0900

    feat(#325): savedStateHandle을 이용해 navigation args 추가, showCreateBoardDialog 추가, deleteBoard 로직 수정

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/boardlist/BoardListViewModel.kt
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/boardlist/BoardListViewModel.kt
@@ -1,9 +1,10 @@
 package boostcamp.and07.mindsync.ui.boardlist
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import boostcamp.and07.mindsync.data.model.Board
 import boostcamp.and07.mindsync.data.repository.boardlist.BoardListRepository
+import boostcamp.and07.mindsync.ui.util.fileToMultiPart
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -13,49 +14,71 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import okhttp3.MultipartBody
+import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
 class BoardListViewModel
     @Inject
     constructor(
+        private val savedStateHandle: SavedStateHandle,
         private val boardListRepository: BoardListRepository,
     ) : ViewModel() {
-        private val _boardUiState = MutableStateFlow(BoardUiState())
-        val boardUiState: StateFlow<BoardUiState> = _boardUiState
-        private val _boardUiEvent = MutableSharedFlow<BoardUiEvent>()
-        val boardUiEvent: SharedFlow<BoardUiEvent> = _boardUiEvent
+        private val _boardUiState = MutableStateFlow(BoardListUiState())
+        val boardUiState: StateFlow<BoardListUiState> = _boardUiState
+        private val _boardUiEvent = MutableSharedFlow<BoardListUiEvent>()
+        val boardUiEvent: SharedFlow<BoardListUiEvent> = _boardUiEvent
+        private lateinit var coroutineExceptionHandler: CoroutineExceptionHandler
 
-        private val coroutineExceptionHandler =
-            CoroutineExceptionHandler { _, throwable ->
-                viewModelScope.launch {
-                    _boardUiEvent.emit(BoardUiEvent.ShowMessage(throwable.message.toString()))
+        init {
+            setCoroutineExceptionHandler()
+            setSpaceId()
+        }
+
+        private fun setCoroutineExceptionHandler() {
+            coroutineExceptionHandler =
+                CoroutineExceptionHandler { _, throwable ->
+                    viewModelScope.launch {
+                        _boardUiEvent.emit(BoardListUiEvent.ShowMessage(throwable.message.toString()))
+                    }
                 }
-            }
+        }
 
-        fun setSpaceId(spaceId: String) {
-            _boardUiState.update { boardUiState ->
-                boardUiState.copy(spaceId = spaceId)
+        private fun setSpaceId() {
+            val spaceId = savedStateHandle.get<String>("spaceId").orEmpty()
+            spaceId?.let {
+                _boardUiState.update { boardUiState ->
+                    boardUiState.copy(spaceId = spaceId)
+                }
+                getBoards()
             }
-            getBoards()
         }
 
         fun addBoard(
-            imageFile: MultipartBody.Part?,
+            imageFile: File?,
             name: String,
         ) {
+            val imageUrl =
+                imageFile?.let {
+                    fileToMultiPart(imageFile, "image")
+                }
             viewModelScope.launch(coroutineExceptionHandler) {
                 boardListRepository.createBoard(
                     boardName = name,
                     spaceId = _boardUiState.value.spaceId,
-                    imageUrl = imageFile,
+                    imageUrl = imageUrl,
                 ).collectLatest { board ->
                     _boardUiState.update { boardUiState ->
                         val newBoards = boardUiState.boards.toMutableList().apply { add(board) }
                         boardUiState.copy(boards = newBoards)
                     }
                 }
+            }
+        }
+
+        fun showCreateBoardDialog(isShown: Boolean) {
+            _boardUiState.update { uiState ->
+                uiState.copy(isShownDialog = isShown)
             }
         }
 
@@ -70,24 +93,25 @@ class BoardListViewModel
             }
         }
 
-    fun selectBoard() {
-        _boardUiState.update { boardUiState ->
-            val newSelectBoards =
-                boardUiState.boards.toMutableList().filter { board -> board.isChecked }
-            boardUiState.copy(
-                selectBoards = newSelectBoards,
-            )
+        fun selectBoard() {
+            _boardUiState.update { boardUiState ->
+                val newSelectBoards =
+                    boardUiState.boards.toMutableList().filter { board -> board.isChecked }
+                boardUiState.copy(
+                    selectBoards = newSelectBoards,
+                )
+            }
         }
-    }
 
         fun deleteBoard() {
             viewModelScope.launch(coroutineExceptionHandler) {
                 val newBoards = boardUiState.value.boards.toMutableList()
                 val newSelectBoards = boardUiState.value.selectBoards.toMutableList()
-                _boardUiState.value.selectBoards.map { board ->
+                _boardUiState.value.boards.filter { board ->
+                    board.isChecked
+                }.map { board ->
                     boardListRepository.deleteBoard(board.id).collectLatest {
                         newBoards.remove(board)
-                        newSelectBoards.remove(board)
                     }
                 }
                 _boardUiState.update { boardUiState ->
