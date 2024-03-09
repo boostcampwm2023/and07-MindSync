3977784b133677d3bcc3de25ed25851b26bdc3c1
dltkd1395 <dltkd13956@naver.com>
2024-03-09 18:45:02 +0900

    feat(#327): BackIconButton 컴포저블 추가

---
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/components/MSIconButton.kt
@@ -0,0 +1,27 @@
+package boostcamp.and07.mindsync.ui.components
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import boostcamp.and07.mindsync.R
+
+@Composable
+fun BackIconButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    iconColor: Color = Color.Black,
+) {
+    IconButton(
+        modifier = modifier,
+        onClick = onClick,
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_back),
+            contentDescription = null,
+            tint = iconColor,
+        )
+    }
+}
