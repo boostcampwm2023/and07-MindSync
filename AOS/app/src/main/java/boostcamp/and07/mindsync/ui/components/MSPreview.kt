df92c538b17becad618f132b54b85a2f6d8efebf
dltkd1395 <dltkd13956@naver.com>
2024-03-09 18:44:06 +0900

    feat(#327): Preview 컴포저블 함수 추가

---
+++ AOS/app/src/main/java/boostcamp/and07/mindsync/ui/components/MSPreview.kt
@@ -0,0 +1,9 @@
+package boostcamp.and07.mindsync.ui.components
+
+import androidx.compose.runtime.Composable
+import boostcamp.and07.mindsync.ui.theme.MindSyncTheme
+
+@Composable
+fun MSPreview(isDarkTheme: Boolean = false, content: @Composable () -> Unit) {
+    MindSyncTheme(darkTheme = isDarkTheme, content = content)
+}
