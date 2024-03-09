272d81b89de7b34999ae6b93f299e477d0866654
jaehan4707 <jahan4707@naver.com>
2024-03-09 20:31:11 +0900

    refactor(#325) - xml , 어뎁터 , 안쓰는 파일 제거

--- AOS/app/src/main/java/boostcamp/and07/mindsync/ui/boardlist/BoardClickListener.kt
+++
@@ -1,9 +0,0 @@
-package boostcamp.and07.mindsync.ui.boardlist
-
-import boostcamp.and07.mindsync.data.model.Board
-
-interface BoardClickListener {
-    fun onClick(board: Board)
-
-    fun onCheckBoxClick(board: Board)
-}
