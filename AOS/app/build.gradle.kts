272039c413fd1a9ff1dfb18e4fccabfe9912b372
dltkd1395 <dltkd13956@naver.com>
2024-03-14 17:53:22 +0900

    release 1.0.2

--- AOS/app/build.gradle.kts
+++ AOS/app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "boostcamp.and07.mindsync"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.0.1"
+        versionCode = 9
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "BASE_URL", "$url")
