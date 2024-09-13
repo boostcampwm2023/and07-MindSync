91f8ad135210b5b023a76a631a46b84c219c81a9
Conut-1 <1mim1@naver.com>
2024-09-13 16:27:00 +0900

    feat: status code 상수로 변경

--- nestjs-BE/server/test/app.e2e-spec.ts
+++ nestjs-BE/server/test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
@@ -22,7 +22,7 @@ describe('AppController (e2e)', () => {
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
-      .expect(200)
+      .expect(HttpStatus.OK)
       .expect('Hello World!');
   });
 });
