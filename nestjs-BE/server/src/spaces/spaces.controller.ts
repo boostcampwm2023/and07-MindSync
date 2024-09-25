af3a3ee3089459e3cc37eeec9f5dc0da40997884
Conut-1 <1mim1@naver.com>
2024-09-25 18:13:04 +0900

    feat: spaces controller v2 추가

--- nestjs-BE/server/src/spaces/spaces.controller.ts
+++ nestjs-BE/server/src/spaces/spaces.controller.ts
@@ -22,6 +22,93 @@ import { RequestWithUser } from '../utils/interface';
 import { ProfilesService } from '../profiles/profiles.service';
 import { ConfigService } from '@nestjs/config';
 
+@Controller('v2/spaces')
+@ApiTags('spaces')
+export class SpacesControllerV2 {
+  constructor(
+    private readonly spacesService: SpacesService,
+    private readonly uploadService: UploadService,
+    private readonly profileSpaceService: ProfileSpaceService,
+    private readonly profilesService: ProfilesService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('icon'))
+  @ApiOperation({ summary: 'Create space' })
+  @ApiResponse({
+    status: 201,
+    description: 'The space has been successfully created.',
+  })
+  async create(
+    @UploadedFile() icon: Express.Multer.File,
+    @Body(new ValidationPipe({ whitelist: true, disableErrorMessages: true }))
+    createSpaceDto: CreateSpaceDto,
+    @Req() req: RequestWithUser,
+  ) {
+    const profile = await this.profilesService.findProfile(req.user.uuid);
+    if (!profile) throw new NotFoundException();
+    const iconUrl = icon
+      ? await this.uploadService.uploadFile(icon)
+      : this.configService.get<string>('APP_ICON_URL');
+    createSpaceDto.icon = iconUrl;
+    const space = await this.spacesService.createSpace(createSpaceDto);
+    await this.profileSpaceService.joinSpace(profile.uuid, space.uuid);
+    return { statusCode: 201, message: 'Created', data: space };
+  }
+
+  @Get(':space_uuid')
+  @ApiOperation({ summary: 'Get space by space_uuid' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return the space data.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Space not found.',
+  })
+  async findOne(@Param('space_uuid') spaceUuid: string) {
+    const space = await this.spacesService.findSpace(spaceUuid);
+    if (!space) throw new NotFoundException();
+    return { statusCode: 200, message: 'Success', data: space };
+  }
+
+  @Patch(':space_uuid')
+  @UseInterceptors(FileInterceptor('icon'))
+  @ApiOperation({ summary: 'Update space by space_uuid' })
+  @ApiResponse({
+    status: 200,
+    description: 'Space has been successfully updated.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad Request. Invalid input data.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Space not found.',
+  })
+  async update(
+    @UploadedFile() icon: Express.Multer.File,
+    @Param('space_uuid') spaceUuid: string,
+    @Body(new ValidationPipe({ whitelist: true, disableErrorMessages: true }))
+    updateSpaceDto: UpdateSpaceDto,
+  ) {
+    if (icon) {
+      updateSpaceDto.icon = await this.uploadService.uploadFile(icon);
+    }
+    const space = await this.spacesService.updateSpace(
+      spaceUuid,
+      updateSpaceDto,
+    );
+    if (!space) throw new NotFoundException();
+    return { statusCode: 200, message: 'Success', data: space };
+  }
+}
+
+/*
+  OLD VERSION
+*/
 @Controller('spaces')
 @ApiTags('spaces')
 export class SpacesController {
