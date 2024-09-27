1355975f77bebc5a478ee8b744e10db20488f054
Conut-1 <1mim1@naver.com>
2024-09-27 20:52:24 +0900

    refactor: 불필요한 테스트 제거
    Prisma의 값을 반환하기만 하는 함수 테스트 제거

--- nestjs-BE/server/src/spaces/spaces.service.spec.ts
+++ nestjs-BE/server/src/spaces/spaces.service.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SpacesService } from './spaces.service';
-import { Space } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
@@ -14,68 +13,13 @@ describe('SpacesService', () => {
         SpacesService,
         {
           provide: PrismaService,
-          useValue: {
-            space: {
-              findUnique: jest.fn(),
-              findMany: jest.fn(),
-              create: jest.fn(),
-              update: jest.fn(),
-              delete: jest.fn(),
-            },
-          },
+          useValue: { space: { update: jest.fn() } },
         },
       ],
     }).compile();
 
     spacesService = module.get<SpacesService>(SpacesService);
     prisma = module.get<PrismaService>(PrismaService);
-  });
-
-  it('findSpace found space', async () => {
-    const spaceMock = { uuid: 'space uuid' } as Space;
-    jest.spyOn(prisma.space, 'findUnique').mockResolvedValue(spaceMock);
-
-    const space = spacesService.findSpace('space uuid');
-
-    await expect(space).resolves.toEqual(spaceMock);
-  });
-
-  it('findSpace not found space', async () => {
-    jest.spyOn(prisma.space, 'findUnique').mockResolvedValue(null);
-
-    const space = spacesService.findSpace('bad space uuid');
-
-    await expect(space).resolves.toBeNull();
-  });
-
-  it('findSpaces found spaces', async () => {
-    const spaceUuidsMock = [
-      { uuid: 'space uuid 1' },
-      { uuid: 'space uuid 2' },
-    ] as Space[];
-    jest.spyOn(prisma.space, 'findMany').mockResolvedValue(spaceUuidsMock);
-
-    const spaces = spacesService.findSpaces(['space uuid 1', 'space uuid 2']);
-
-    await expect(spaces).resolves.toEqual(spaceUuidsMock);
-  });
-
-  it('findSpaces not found spaces', async () => {
-    jest.spyOn(prisma.space, 'findMany').mockResolvedValue([]);
-
-    const spaces = spacesService.findSpaces(['space uuid 1', 'space uuid 2']);
-
-    await expect(spaces).resolves.toHaveLength(0);
-  });
-
-  it('createSpace created space', async () => {
-    const data = { name: 'new space name', icon: 'new icon' };
-    const spaceMock = { uuid: 'space uuid', ...data };
-    jest.spyOn(prisma.space, 'create').mockResolvedValue(spaceMock);
-
-    const space = spacesService.createSpace(data);
-
-    await expect(space).resolves.toEqual(spaceMock);
   });
 
   it('updateSpace updated space', async () => {
@@ -102,29 +46,5 @@ describe('SpacesService', () => {
     const space = spacesService.updateSpace('space uuid', data);
 
     await expect(space).resolves.toBeNull();
-  });
-
-  it('deleteSpace deleted spaced', async () => {
-    const spaceMock = { uuid: 'space uuid' } as Space;
-    jest.spyOn(prisma.space, 'delete').mockResolvedValue(spaceMock);
-
-    const space = spacesService.deleteSpace('space uuid');
-
-    await expect(space).resolves.toEqual(spaceMock);
-  });
-
-  it('deleteSpace fail', async () => {
-    jest
-      .spyOn(prisma.space, 'delete')
-      .mockRejectedValue(
-        new PrismaClientKnownRequestError(
-          'An operation failed because it depends on one or more records that were required but not found. Record to delete not found.',
-          { code: 'P2025', clientVersion: '' },
-        ),
-      );
-
-    const space = spacesService.deleteSpace('space uuid');
-
-    await expect(space).rejects.toThrow(PrismaClientKnownRequestError);
   });
 });
