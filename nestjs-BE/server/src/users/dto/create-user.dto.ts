0acf02ee0a27c1c2fdccbe87049222adf7df00b7
Conut-1 <1mim1@naver.com>
2024-10-02 17:02:52 +0900

    refactor: DTO 이름 변경

--- nestjs-BE/server/src/users/dto/create-user.dto.ts
+++ nestjs-BE/server/src/users/dto/create-user.dto.ts
@@ -1,3 +1,3 @@
-export class CreateUserDto {
+export class CreateUserPrismaDto {
   readonly email: string;
 }
--- nestjs-BE/server/src/users/users.service.ts
+++ nestjs-BE/server/src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserPrismaDto } from './dto/create-user.dto';
 import { User } from '@prisma/client';
 import { v4 as uuid } from 'uuid';
 
@@ -8,7 +8,7 @@ import { v4 as uuid } from 'uuid';
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async getOrCreateUser(data: CreateUserDto): Promise<User> {
+  async getOrCreateUser(data: CreateUserPrismaDto): Promise<User> {
     return this.prisma.$transaction(async () => {
       const kakaoUser = await this.prisma.kakaoUser.findUnique({
         where: { email: data.email },
