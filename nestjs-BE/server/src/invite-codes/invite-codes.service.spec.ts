147e682f7239453fe49a9118ef3c70f27d62e897
Conut-1 <1mim1@naver.com>
2024-11-25 21:22:54 +0900

    test: findSpace 테스트

---
+++ nestjs-BE/server/src/invite-codes/invite-codes.service.spec.ts
@@ -0,0 +1,92 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HttpException, NotFoundException } from '@nestjs/common';
+import { InviteCode, Space } from '@prisma/client';
+import { InviteCodesService } from './invite-codes.service';
+import { SpacesService } from '../spaces/spaces.service';
+import { PrismaService } from '../prisma/prisma.service';
+import * as ExpiryModule from '../utils/date';
+
+describe('InviteCodesService', () => {
+  let inviteCodesService: InviteCodesService;
+  let spacesService: SpacesService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        InviteCodesService,
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+        {
+          provide: SpacesService,
+          useValue: {
+            findSpaceBySpaceUuid: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    inviteCodesService = module.get<InviteCodesService>(InviteCodesService);
+    spacesService = module.get<SpacesService>(SpacesService);
+  });
+
+  describe('findSpace', () => {
+    const testSpace: Space = {
+      uuid: 'test uuid',
+      name: 'test space',
+      icon: 'test icon',
+    };
+    const testInviteCode = 'test invite code';
+    const testInviteCodeDate = {
+      spaceUuid: 'space uuid',
+      expiryDate: 'expriy date',
+    } as unknown as InviteCode;
+    let checkExpirySpy: jest.SpyInstance;
+    let deleteInviteCodeSpy: jest.SpyInstance;
+
+    beforeEach(() => {
+      jest
+        .spyOn(inviteCodesService, 'findInviteCode')
+        .mockResolvedValue(testInviteCodeDate);
+      checkExpirySpy = jest
+        .spyOn(ExpiryModule, 'checkExpiry')
+        .mockReturnValue(false);
+      deleteInviteCodeSpy = jest
+        .spyOn(inviteCodesService, 'deleteInviteCode')
+        .mockResolvedValue(null);
+      (spacesService.findSpaceBySpaceUuid as jest.Mock).mockResolvedValue(
+        testSpace,
+      );
+    });
+
+    afterEach(() => {
+      checkExpirySpy.mockRestore();
+    });
+
+    it('success', async () => {
+      const space = inviteCodesService.findSpace(testInviteCode);
+
+      await expect(space).resolves.toEqual(testSpace);
+      expect(deleteInviteCodeSpy).not.toHaveBeenCalled();
+    });
+
+    it('invite code not found', async () => {
+      jest.spyOn(inviteCodesService, 'findInviteCode').mockResolvedValue(null);
+
+      const space = inviteCodesService.findSpace(testInviteCode);
+
+      await expect(space).rejects.toThrow(NotFoundException);
+      expect(checkExpirySpy).not.toHaveBeenCalled();
+    });
+
+    it('invite code expired', async () => {
+      checkExpirySpy.mockReturnValue(true);
+
+      const space = inviteCodesService.findSpace(testInviteCode);
+
+      await expect(space).rejects.toThrow(HttpException);
+      expect(deleteInviteCodeSpy).toHaveBeenCalled();
+    });
+  });
+});
