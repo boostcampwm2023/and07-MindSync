2244e2e1c2064fe6d6bc8f63c9f5c69ffd96ace7
Conut-1 <1mim1@naver.com>
2025-02-03 21:25:35 +0900

    fix: gateway에서 트리 조작 제거

--- nestjs-BE/server/src/board-trees/board-trees.gateway.ts
+++ nestjs-BE/server/src/board-trees/board-trees.gateway.ts
@@ -8,12 +8,6 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { BoardTreesService } from './board-trees.service';
-import {
-  OperationAdd,
-  OperationDelete,
-  OperationMove,
-  OperationUpdate,
-} from '../crdt/operation';
 import type { BoardOperation } from './schemas/board-operation.schema';
 
 @WebSocketGateway({ namespace: 'board' })
@@ -42,39 +36,8 @@ export class BoardTreesGateway implements OnGatewayConnection {
   @SubscribeMessage('joinBoard')
   async handleJoinBoard(client: Socket, payload: string) {
     const payloadObject = JSON.parse(payload);
-    if (!this.boardTreesService.hasTree(payloadObject.boardId)) {
-      await this.boardTreesService.initBoardTree(
-        payloadObject.boardId,
-        payloadObject.boardName,
-      );
-    }
     client.join(payloadObject.boardId);
-    client.emit(
-      'initTree',
-      this.boardTreesService.getTreeData(payloadObject.boardId),
-    );
-  }
-
-  @SubscribeMessage('updateMindmap')
-  handleUpdateMindmap(client: Socket, payload: string) {
-    const payloadObject = JSON.parse(payload);
-    const { boardId, operation: serializedOperation } = payloadObject;
-
-    const operationTypeMap = {
-      add: OperationAdd.parse,
-      delete: OperationDelete.parse,
-      move: OperationMove.parse,
-      update: OperationUpdate.parse,
-    };
-
-    const operation =
-      operationTypeMap[serializedOperation.operationType](serializedOperation);
-    this.boardTreesService.applyOperation(boardId, operation);
-    this.boardTreesService.updateTreeData(boardId);
-
-    client.broadcast
-      .to(boardId)
-      .emit('operationFromServer', serializedOperation);
+    client.emit('boardJoined');
   }
 
   @SubscribeMessage('createOperation')
