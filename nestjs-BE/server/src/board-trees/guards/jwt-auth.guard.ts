aaec470747932f443be55bbb3e22acf831fb8d44
Conut-1 <1mim1@naver.com>
2025-02-11 17:39:31 +0900

    feat: websocket용 JwtAuth 가드

---
+++ nestjs-BE/server/src/board-trees/guards/jwt-auth.guard.ts
@@ -0,0 +1,27 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { WsException } from '@nestjs/websockets';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToWs().getData();
+    if (!request.token) {
+      throw new WsException('access token required');
+    }
+    try {
+      this.jwtService.verify(request.token, {
+        secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
+      });
+    } catch (error) {
+      throw new WsException('access token invalid');
+    }
+    return true;
+  }
+}
