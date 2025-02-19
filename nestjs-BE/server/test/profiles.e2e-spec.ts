2b5f10a263087da21d5607c442f97a1abe275968
Conut-1 <1mim1@naver.com>
2025-02-19 21:02:27 +0900

    test: /profiles/:profile_uuid PATCH 테스트 추가

--- nestjs-BE/server/test/profiles.e2e-spec.ts
+++ nestjs-BE/server/test/profiles.e2e-spec.ts
@@ -1,7 +1,10 @@
 import { HttpStatus } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test } from '@nestjs/testing';
+import { readFile } from 'fs/promises';
 import { sign } from 'jsonwebtoken';
+import { omit } from 'lodash';
+import { resolve } from 'path';
 import * as request from 'supertest';
 import { v4 as uuid } from 'uuid';
 import { AuthModule } from '../src/auth/auth.module';
@@ -103,7 +106,100 @@ describe('ProfilesController (e2e)', () => {
         .expect({
           statusCode: HttpStatus.OK,
           message: 'Success',
-          data: testProfile,
+          data: omit(testProfile, ['userUuid']),
+        });
+    });
+  });
+
+  describe('/profiles (PATCH)', () => {
+    let testToken: string;
+    let testProfile: Profile;
+    let path: string;
+
+    beforeEach(async () => {
+      const testUser = await createUser(prisma);
+      testToken = createToken(testUser.uuid, config);
+      testProfile = await createProfile(testUser.uuid, prisma);
+      path = `/profiles/${testProfile.uuid}`;
+    });
+
+    it('success without update', () => {
+      return request(app.getHttpServer())
+        .patch(path)
+        .auth(testToken, { type: 'bearer' })
+        .expect(HttpStatus.OK)
+        .expect({
+          statusCode: HttpStatus.OK,
+          message: 'Success',
+          data: omit(testProfile, ['userUuid']),
+        });
+    });
+
+    it('success nickname update', () => {
+      const newNickname = 'new nickname';
+
+      return request(app.getHttpServer())
+        .patch(path)
+        .auth(testToken, { type: 'bearer' })
+        .send({ nickname: newNickname })
+        .expect(HttpStatus.OK)
+        .expect({
+          statusCode: HttpStatus.OK,
+          message: 'Success',
+          data: {
+            ...omit(testProfile, ['userUuid']),
+            nickname: newNickname,
+          },
+        });
+    });
+
+    it('success image update', async () => {
+      const imageUrlPattern = `^https\\:\\/\\/${config.get<string>(
+        'S3_BUCKET_NAME',
+      )}\\.s3\\.${config.get<string>(
+        'AWS_REGION',
+      )}\\.amazonaws\\.com\\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}-`;
+      const imageRegExp = new RegExp(imageUrlPattern);
+      const testImage = await readFile(resolve(__dirname, './base_image.png'));
+
+      return request(app.getHttpServer())
+        .patch(path)
+        .auth(testToken, { type: 'bearer' })
+        .attach('image', testImage, 'base_image.png')
+        .expect(HttpStatus.OK)
+        .expect((res) => {
+          expect(res.body.message).toBe('Success');
+          expect(res.body.statusCode).toBe(HttpStatus.OK);
+          expect(res.body.data.uuid).toBe(testProfile.uuid);
+          expect(res.body.data.userUuid).toBeUndefined();
+          expect(res.body.data.image).toMatch(imageRegExp);
+          expect(res.body.data.nickname).toBe(testProfile.nickname);
+        });
+    });
+
+    it('success update all', async () => {
+      const newNickname = 'new nickname';
+      const imageUrlPattern = `^https\\:\\/\\/${config.get<string>(
+        'S3_BUCKET_NAME',
+      )}\\.s3\\.${config.get<string>(
+        'AWS_REGION',
+      )}\\.amazonaws\\.com\\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}-`;
+      const imageRegExp = new RegExp(imageUrlPattern);
+      const testImage = await readFile(resolve(__dirname, './base_image.png'));
+
+      return request(app.getHttpServer())
+        .patch(path)
+        .auth(testToken, { type: 'bearer' })
+        .attach('image', testImage, 'base_image.png')
+        .field('nickname', newNickname)
+        .expect(HttpStatus.OK)
+        .expect((res) => {
+          expect(res.body.message).toBe('Success');
+          expect(res.body.statusCode).toBe(HttpStatus.OK);
+          expect(res.body.data.uuid).toBe(testProfile.uuid);
+          expect(res.body.data.userUuid).toBeUndefined();
+          expect(res.body.data.image).toMatch(imageRegExp);
+          expect(res.body.data.nickname).toBe(newNickname);
         });
     });
   });
