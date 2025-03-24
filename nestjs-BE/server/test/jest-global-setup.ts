65e57c58dece519d383bf4a712d1df7aed1d1406
Conut-1 <1mim1@naver.com>
2025-03-24 20:06:09 +0900

    feat: localstack testcontainers로 생성

--- nestjs-BE/server/test/jest-global-setup.ts
+++ nestjs-BE/server/test/jest-global-setup.ts
@@ -1,9 +1,31 @@
 import { MongoDBContainer } from '@testcontainers/mongodb';
+import { LocalstackContainer } from '@testcontainers/localstack';
+import { CreateBucketCommand, S3Client } from '@aws-sdk/client-s3';
 
 export default async function () {
   const mongoDBContainer = await new MongoDBContainer()
     .withExposedPorts({ host: 27019, container: 27017 })
     .start();
 
+  const localStackContainer = await new LocalstackContainer()
+    .withExposedPorts({ host: 4566, container: 4566 })
+    .start();
+
+  const client = new S3Client({
+    endpoint: 'http://localhost:4566',
+    forcePathStyle: true,
+    region: 'ap-northeast-2',
+    credentials: {
+      accessKeyId: 'test',
+      secretAccessKey: 'test',
+    },
+  });
+  const createBucketCommand = new CreateBucketCommand({
+    Bucket: 'test-bucket',
+  });
+  await client.send(createBucketCommand);
+  client.destroy();
+
   globalThis.mongodb = mongoDBContainer;
+  globalThis.localstack = localStackContainer;
 }
--- nestjs-BE/server/test/jest-global-teardown.ts
+++ nestjs-BE/server/test/jest-global-teardown.ts
@@ -1,3 +1,4 @@
 export default async function () {
   await globalThis.mongodb.stop();
+  await globalThis.localstack.stop();
 }
